c5f82d278e7a438940e5de2a553a670e6c32506b
Agustin Aguilar <Agusxrun@gmail.com>
2020-08-27 12:42:08 +0200

    Remote signers

---
+++ packages/provider/src/signers/remote-signer.ts
@@ -0,0 +1,20 @@
+
+import { Signer as AbstractSigner } from 'ethers'
+import { Arrayish } from 'ethers/utils'
+import { TransactionRequest, TransactionResponse } from 'ethers/providers'
+
+export abstract class RemoteSigner extends AbstractSigner {
+  signMessage(message: Arrayish): Promise<string> {
+    return this.signMessageWithData(message)
+  }
+
+  abstract signMessageWithData(message: Arrayish, data?: Arrayish): Promise<string>
+
+  sendTransaction(_: TransactionRequest): Promise<TransactionResponse> {
+    throw new Error("sendTransaction not implemented.")
+  }
+
+  static isRemoteSigner(signer: AbstractSigner): boolean {
+    return (<RemoteSigner>signer).signMessageWithData !== undefined
+  }
+}
