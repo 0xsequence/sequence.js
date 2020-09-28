c6d0349cc149af4e649197005b6720e73715a0a7
Peter Kieltyka <peter.kieltyka@gmail.com>
2020-09-28 09:21:20 -0400

    provider: export WalletProviderConfig

--- packages/provider/src/index.ts
+++ packages/provider/src/index.ts
@@ -39,7 +39,7 @@ export { JsonRpcAsyncSender } from './providers/async-sender'
 
 export { ExternalWindowProvider } from './providers/external-window-provider'
 
-export { WalletProvider } from './providers/wallet-provider'
+export { WalletProvider, WalletProviderConfig } from './providers/wallet-provider'
 
 export { WalletContext } from './context'
 
