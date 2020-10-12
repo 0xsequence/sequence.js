07a138642905ffa3e374394c2f00daad723ba110
PhABC <ph.castonguay@gmail.com>
2020-10-12 14:51:53 -0400

    Typing generation for deployer contracts

---
+++ packages/deployer/src/utils/configLoader.ts
@@ -0,0 +1,48 @@
+import * as dotenv from 'dotenv'
+import * as path from 'path'
+import { HttpNetworkConfig } from '@nomiclabs/buidler/types'
+import { ethers } from 'ethers'
+
+type EthereumNetworksTypes =
+  | 'rinkeby'
+  | 'ropsten'
+  | 'kovan'
+  | 'goerli'
+  | 'mainnet'
+  | 'mumbai'
+  | 'matic'
+
+export const getEnvConfig = (env: string) => {
+  const envFile = path.resolve(__dirname, `../../config/${env}.env`)
+  const envLoad = dotenv.config({ path: envFile })
+
+  if (envLoad.error) {
+    console.warn("No config found, using default")
+    return { 'ETH_MNEMONIC': ethers.Wallet.createRandom().mnemonic.phrase }
+  }
+
+  return envLoad.parsed || {}
+}
+
+export const networkConfig = (
+  network: EthereumNetworksTypes
+): HttpNetworkConfig => {
+  const config = getEnvConfig('PROD')  
+  const networkConfig: HttpNetworkConfig = {
+    url: (function(network) {
+      switch(network) {
+        case 'mumbai':
+          return 'https://rpc-mumbai.matic.today/'
+
+        case 'matic':
+          return 'https://rpc-mainnet.matic.network'
+
+        default:
+          return `https://${network}.infura.io/v3/${config['INFURA_API_KEY']}`
+      }
+    })(network),
+    accounts: [config['ETH_MNEMONIC']]
+  }
+
+  return networkConfig
+}
