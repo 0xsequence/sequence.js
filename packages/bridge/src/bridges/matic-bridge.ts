86dd12e775afdfba1fd8cec8d4f144ed180c0403
Agustin Aguilar <Agusxrun@gmail.com>
2020-12-03 18:03:29 +0000

    Split Matic bridge into PoS and plasma

--- packages/bridge/src/bridges/matic-bridge.ts
+++ packages/bridge/src/bridges/matic-bridge.ts
@@ -1,6 +1,6 @@
 import { BigNumberish, ethers, providers } from "ethers"
 import { Interface } from "ethers/lib/utils"
-import { Bridge, Move, MoveERC20, MoveEstimate, MoveNative } from "."
+import { Bridge, BridgeERC20, BridgeNative, Move, MoveERC20, MoveEstimate, MoveNative } from "."
 import { ERC_20_ABI } from "../abi/erc20-abi"
 import { MATIC_BRIDGE_ABI } from "../abi/matic-abi"
 import { MaticPOSClient } from '@maticnetwork/maticjs'
@@ -19,16 +19,11 @@ type MaticBridgeConf = {
   etherPredicateProxy: string
 }
 
-export class MaticBridge implements Bridge {
+export class MaticPosBridge implements BridgeNative, BridgeERC20 {
   private static DEPOSIT_TOPIC = "0x9b217a401a5ddf7c4d474074aff9958a18d48690d77cc2151c4706aa7348b401"
   private static TRANSFER_TOPIC = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
   private static DEPOSIT_ETH_TOPIC = "0x3e799b2d61372379e767ef8f04d65089179b7a6f63f9be3065806456c7309f1b"
-  private static DEPOSIT_PLASMA_TOPIC = "0x1dadc8d0683c6f9824e885935c1bec6f76816730dcec148dda8cf25a7b9f797b"
 
-  private static MATIC_NATIVE_TOKEN = "0x0000000000000000000000000000000000001010"
-
-  private static PLASMA_TIME_DEPOSIT = 10 * 60
-  private static PLASMA_TIME_WITHDRAW = 7 * 24 * 60 * 60
   private static POS_TIME_DEPOSIT = 10 * 60
   private static POS_TIME_WITHDRAW = 30 * 60
 
@@ -50,14 +45,14 @@ export class MaticBridge implements Bridge {
   constructor(private conf: MaticBridgeConf) {}
 
   name(): string {
-    return "Matic bridge"
+    return "Matic PoS bridge"
   }
 
   id(): string {
-    return `matic-${this.conf.parentId}-${this.conf.maticId}`
+    return `matic-pos-${this.conf.parentId}-${this.conf.maticId}`
   }
 
-  connect(networks: NetworkConfig[]): MaticBridge {
+  connect(networks: NetworkConfig[]): MaticPosBridge {
     this.maticNet = networks.find((n) => n.chainId === this.conf.maticId)
     this.parentNet = networks.find((n) => n.chainId === this.conf.parentId)
     return this
@@ -113,31 +108,20 @@ export class MaticBridge implements Bridge {
     return flatten(await Promise.all([
       this._depositsNative(wallet, from, to),
       this._depositsERC20(wallet, from, to),
-      this._withdrawsERC20(wallet, from, to),
-      this._depositsERC20Plasma(wallet, from, to)
+      this._withdrawsERC20(wallet, from, to)
     ].map((p) => safeSolve(p, []))))
   }
 
   async supportsNative(from: NetworkConfig, to: NetworkConfig): Promise<boolean> {
-    return this.isWithdraw(from, to) // || this.isDeposit(from, to)
+    return this.isDeposit(from, to)
   }
 
   async estimateNative(from: NetworkConfig, to: NetworkConfig): Promise<MoveEstimate | undefined> {
     if (this.isDeposit(from, to)) {
       return {
-        crossTime: MaticBridge.POS_TIME_DEPOSIT,
+        crossTime: MaticPosBridge.POS_TIME_DEPOSIT,
         steps: 1
       }
-    }
-
-    if (this.isWithdraw(from, to)) {
-      return undefined
-
-      // TODO: Re-enable handle complete
-      // return {
-      //   crossTime: MaticBridge.PLASMA_TIME_WITHDRAW,
-      //   steps: 2
-      // }
     }
 
     return undefined
@@ -151,19 +135,11 @@ export class MaticBridge implements Bridge {
         value: amount
       }]
     }
-  
-    if (this.isWithdraw(from, to)) {
-      return [{
-        to: MaticBridge.MATIC_NATIVE_TOKEN,
-        data: new Interface(MATIC_BRIDGE_ABI).encodeFunctionData('withdraw', [amount])
-      }]
-    }
 
     return undefined
   }
 
-  completeNative(from: NetworkConfig, to: NetworkConfig, txHash: string, wallet: string): Promise<providers.TransactionRequest[]> {
-    // TODO: Implement Plasma withdraw using Matic.js
+  completeNative(_from: NetworkConfig, _to: NetworkConfig, _txHash: string, _wallet: string): Promise<providers.TransactionRequest[]> {
     return undefined
   }
 
@@ -175,9 +151,9 @@ export class MaticBridge implements Bridge {
     const candidates = await parentProvider.getLogs({
       fromBlock: from,
       toBlock: to,
-      address: this.conf.erc20Predicate,
+      address: this.conf.etherPredicateProxy,
       topics: [
-        MaticBridge.DEPOSIT_ETH_TOPIC,
+        MaticPosBridge.DEPOSIT_ETH_TOPIC,
         ethers.utils.defaultAbiCoder.encode(['address'], [wallet]),
         ethers.utils.defaultAbiCoder.encode(['address'], [wallet])
       ]
@@ -185,7 +161,7 @@ export class MaticBridge implements Bridge {
 
     return Promise.all(candidates.map(async (cand) => {
       const stateSyncId = await this.getStateSyncId(cand.transactionHash, cand.logIndex + 1)
-      const isPending = stateSyncId.lt(await lastStateId)
+      const isPending = stateSyncId.gte(await lastStateId)
 
       return {
         ...cand,
@@ -194,7 +170,6 @@ export class MaticBridge implements Bridge {
         isPending: isPending,
         fromChain: this.parentNet.chainId,
         toChain: this.maticNet.chainId,
-        token: cand.address,
         amount: ethers.utils.defaultAbiCoder.decode(['uint256'], cand.data)[0]
       }
     }))
@@ -206,20 +181,24 @@ export class MaticBridge implements Bridge {
 
   async supportsERC20(from: NetworkConfig, to: NetworkConfig, token: string): Promise<boolean> {
     // TODO: Check if token is mapped on Matic
-    return await this.moveERC20(from, to, token, undefined, 0) !== undefined
+    return !this.isMaticToken(token) && await this.moveERC20(from, to, token, ethers.constants.AddressZero, 0) !== undefined
   }
 
   async estimateERC20(from: NetworkConfig, to: NetworkConfig, token: string): Promise<MoveEstimate> {
+    if (this.isMaticToken(token)) {
+      return undefined
+    }
+
     if (this.isDeposit(from, to)) {
       return {
-        crossTime: MaticBridge.POS_TIME_DEPOSIT,
+        crossTime: MaticPosBridge.POS_TIME_DEPOSIT,
         steps: 1
       }
     }
 
     if (this.isWithdraw(from, to)) {
       return {
-        crossTime: MaticBridge.POS_TIME_WITHDRAW,
+        crossTime: MaticPosBridge.POS_TIME_WITHDRAW,
         steps: 2
       }
     }
@@ -228,26 +207,25 @@ export class MaticBridge implements Bridge {
   }
 
   async moveERC20(from: NetworkConfig, to: NetworkConfig, token: string, dest: string, amount: BigNumberish): Promise<providers.TransactionRequest[]> {
+    if (this.isMaticToken(token)) {
+      // Matic token must use the plasma bridge
+      return undefined
+    }
+
     if (this.isDeposit(from, to)) {
-      if (this.isMaticToken(token)) {
-        // Use Plasma bridge
-        return [{
-          to: token,
-          data: new Interface(ERC_20_ABI).encodeFunctionData('approve', [this.conf.depositManager, amount])
-        }, {
-          to: this.conf.depositManager,
-          data: new Interface(MATIC_BRIDGE_ABI).encodeFunctionData('depositERC20ForUser', [this.conf.maticToken, dest, amount])
-        }]
-      } else {
-        // Use PoS bridge
-        return [{
-          to: token,
-          data: new Interface(ERC_20_ABI).encodeFunctionData('approve', [this.conf.erc20Predicate, amount])
-        }, {
-          to: this.conf.rootChainManager,
-          data: new Interface(MATIC_BRIDGE_ABI).encodeFunctionData('depositERC20ForUser', [this.conf.maticToken, dest, amount])
-        }]
-      }
+      return [{
+        to: token,
+        data: new Interface(ERC_20_ABI).encodeFunctionData('approve', [this.conf.erc20Predicate, amount]),
+        gasLimit: 75000
+      }, {
+        to: this.conf.rootChainManager,
+        gasLimit: 300000,
+        data: new Interface(MATIC_BRIDGE_ABI).encodeFunctionData('depositFor', [
+          dest,
+          token,
+          ethers.utils.defaultAbiCoder.encode(['uint256'], [amount])
+        ])
+      }]
     }
 
     if (this.isWithdraw(from, to)) {
@@ -261,7 +239,9 @@ export class MaticBridge implements Bridge {
   }
 
   async completeERC20(_from: NetworkConfig, _to: NetworkConfig, txHash: string, wallet: string): Promise<providers.TransactionRequest[]> {
-    return [await this.posClient.exitERC20(txHash, { from: wallet, encodeAbi: true })]
+    const tx = await this.posClient.exitERC20(txHash, { from: wallet, encodeAbi: true })
+    delete tx.nonce
+    return [tx]
   }
 
   async _depositsERC20(wallet: string, from: providers.BlockTag, to: providers.BlockTag): Promise<MoveERC20[]> {
@@ -274,7 +254,7 @@ export class MaticBridge implements Bridge {
       toBlock: to,
       address: this.conf.erc20Predicate,
       topics: [
-        MaticBridge.DEPOSIT_TOPIC,
+        MaticPosBridge.DEPOSIT_TOPIC,
         ethers.utils.defaultAbiCoder.encode(['address'], [wallet]),
         ethers.utils.defaultAbiCoder.encode(['address'], [wallet])
       ]
@@ -282,8 +262,7 @@ export class MaticBridge implements Bridge {
 
     return Promise.all(candidates.map(async (cand) => {
       const stateSyncId = await this.getStateSyncId(cand.transactionHash, cand.logIndex + 2)
-      const isPending = stateSyncId.lt(await lastStateId)
-
+      const isPending = stateSyncId.gte(await lastStateId)
       return {
         ...cand,
         completeTx: undefined,
@@ -291,39 +270,7 @@ export class MaticBridge implements Bridge {
         isPending: isPending,
         fromChain: this.parentNet.chainId,
         toChain: this.maticNet.chainId,
-        token: cand.address,
-        amount: ethers.utils.defaultAbiCoder.decode(['uint256'], cand.data)[0]
-      }
-    }))
-  }
-
-  async _depositsERC20Plasma(wallet: string, from: providers.BlockTag, to: providers.BlockTag): Promise<MoveERC20[]> {
-    const parentProvider = new ethers.providers.JsonRpcProvider(this.parentNet.rpcUrl)
-
-    const lastStateId = this.lastStateId()
-
-    const candidates = await parentProvider.getLogs({
-      fromBlock: from,
-      toBlock: to,
-      address: this.conf.depositManager,
-      topics: [
-        MaticBridge.DEPOSIT_PLASMA_TOPIC,
-        ethers.utils.defaultAbiCoder.encode(['address'], [wallet])
-      ]
-    })
-
-    return Promise.all(candidates.map(async (cand) => {
-      const stateSyncId = await this.getStateSyncId(cand.transactionHash, cand.logIndex - 1)
-      const isPending = stateSyncId.lt(await lastStateId)
-
-      return {
-        ...cand,
-        completeTx: undefined,
-        isCompleted: !isPending,
-        isPending: isPending,
-        fromChain: this.parentNet.chainId,
-        toChain: this.maticNet.chainId,
-        token: cand.address,
+        token: ethers.utils.defaultAbiCoder.decode(['address'], cand.topics[3])[0],
         amount: ethers.utils.defaultAbiCoder.decode(['uint256'], cand.data)[0]
       }
     }))
@@ -336,26 +283,15 @@ export class MaticBridge implements Bridge {
       fromBlock: from,
       toBlock: to,
       topics: [
-        MaticBridge.TRANSFER_TOPIC,
+        MaticPosBridge.TRANSFER_TOPIC,
         ethers.utils.defaultAbiCoder.encode(['address'], [wallet]),
         ethers.utils.defaultAbiCoder.encode(['address'], [ethers.constants.AddressZero])
       ]
     })
 
     return Promise.all(candidates.map(async (cand) => {
-      const isCompleted = (async () => {
-        try {
-          return await this.posClient.isERC20ExitProcessed(cand.transactionHash)
-        } catch {}
-        return false
-      })()
-  
-      const completeTx = (async () => {
-        try {
-          return await this.completeERC20(this.maticNet, this.parentNet, wallet, cand.transactionHash)
-        } catch {}
-        return undefined
-      })()
+      const isCompleted = safeSolve(this.posClient.isERC20ExitProcessed(cand.transactionHash), false)
+      const completeTx = safeSolve(this.completeERC20(this.maticNet, this.parentNet, cand.transactionHash, wallet), undefined)
 
       return {
         ...cand,
@@ -368,46 +304,6 @@ export class MaticBridge implements Bridge {
         amount: ethers.utils.defaultAbiCoder.decode(['uint256'], cand.data)[0]
       }
     }))
-  }
-
-  // ///
-  // ERC1155
-  // ///
-
-  supportsERC1155(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<boolean> {
-    throw new Error("Method not implemented.")
-  }
-
-  estimateERC1155(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<MoveEstimate> {
-    throw new Error("Method not implemented.")
-  }
-
-  moveERC1155(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[], amounts: BigNumberish[]): Promise<providers.TransactionRequest[]> {
-    throw new Error("Method not implemented.")
-  }
-
-  completeERC1155(from: NetworkConfig, to: NetworkConfig, txHash: string, wallet: string): Promise<providers.TransactionRequest[]> {
-    throw new Error("Method not implemented.")
-  }
-
-  // ///
-  // ERC721
-  // ///
-
-  supportsERC721(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<boolean> {
-    throw new Error("Method not implemented.")
-  }
-
-  estimateERC721(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<MoveEstimate> {
-    throw new Error("Method not implemented.")
-  }
-
-  moveERC721(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<providers.TransactionRequest[]> {
-    throw new Error("Method not implemented.")
-  }
-
-  completeERC721(from: NetworkConfig, to: NetworkConfig, txHash: string, wallet: string): Promise<providers.TransactionRequest[]> {
-    throw new Error("Method not implemented.")
   }
 
   // ///
