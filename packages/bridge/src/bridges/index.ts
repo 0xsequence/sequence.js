a3e7ed7d4f2b28513d08c92e4d80ecd18f226fb1
Agustin Aguilar <Agusxrun@gmail.com>
2020-12-03 18:02:18 +0000

    Split bridge types

--- packages/bridge/src/bridges/index.ts
+++ packages/bridge/src/bridges/index.ts
@@ -4,8 +4,8 @@ import { NetworkConfig } from '@arcadeum/provider'
 import { BigNumberish, providers } from 'ethers'
 
 export type MoveEstimate = {
-  crossTime: BigNumberish,
-  steps: BigNumberish
+  crossTime: number,
+  steps: number
 }
 
 export type Move = {
@@ -47,7 +47,7 @@ export function isERC721Move(move: Move): move is MoveERC721 {
   return cand.token !== undefined && cand.id !== undefined && !cand.amount
 }
 
-export function isERC1155Move(move: Move): move is MoveERC721 {
+export function isERC1155Move(move: Move): move is MoveERC1155 {
   const cand = move as MoveERC1155
   return cand.token !== undefined && cand.id !== undefined && cand.amount !== undefined
 }
@@ -64,24 +64,52 @@ export interface Bridge {
   connect(networks: NetworkConfig[]): Bridge
 
   getMoves(wallet: string, from: providers.BlockTag, to: providers.BlockTag): Promise<Move[]>
+}
 
+export interface BridgeNative extends Bridge {
   supportsNative(from: NetworkConfig, to: NetworkConfig): Promise<boolean>
   estimateNative(from: NetworkConfig, to: NetworkConfig): Promise<MoveEstimate | undefined>
   moveNative(from: NetworkConfig, to: NetworkConfig, dest: string, amount: BigNumberish): Promise<providers.TransactionRequest[] | undefined>
   completeNative(from: NetworkConfig, to: NetworkConfig, txHash: string, wallet: string): Promise<providers.TransactionRequest[] | undefined>
+}
 
+export function isBridgeNative(bridge: Bridge): bridge is BridgeNative {
+  const cand = bridge as BridgeNative
+  return cand.supportsNative !== undefined && cand.estimateNative !== undefined && cand.moveNative !== undefined && cand.completeNative !== undefined
+}
+
+export interface BridgeERC20 extends Bridge {
   supportsERC20(from: NetworkConfig, to: NetworkConfig, token: string): Promise<boolean>
   estimateERC20(from: NetworkConfig, to: NetworkConfig, token: string): Promise<MoveEstimate | undefined>
   moveERC20(from: NetworkConfig, to: NetworkConfig, token: string, dest: string, amount: BigNumberish): Promise<providers.TransactionRequest[] | undefined>
   completeERC20(from: NetworkConfig, to: NetworkConfig, txHash: string, wallet: string): Promise<providers.TransactionRequest[] | undefined>
+}
 
-  supportsERC1155(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<boolean>
-  estimateERC1155(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<MoveEstimate | undefined>
-  moveERC1155(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[], amounts: BigNumberish[]): Promise<providers.TransactionRequest[] | undefined>
-  completeERC1155(from: NetworkConfig, to: NetworkConfig, txHash: string, wallet: string): Promise<providers.TransactionRequest[] | undefined>
+export function isBridgeERC20(bridge: Bridge): bridge is BridgeERC20 {
+  const cand = bridge as BridgeERC20
+  return cand.supportsERC20 !== undefined && cand.estimateERC20 !== undefined && cand.moveERC20 !== undefined && cand.completeERC20 !== undefined
+}
 
+export interface BridgeERC721 extends Bridge {
   supportsERC721(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<boolean>
   estimateERC721(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<MoveEstimate | undefined>
   moveERC721(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<providers.TransactionRequest[] | undefined>
   completeERC721(from: NetworkConfig, to: NetworkConfig, txHash: string, wallet: string): Promise<providers.TransactionRequest[] | undefined>
+}
+
+export function isBridgeERC712(bridge: Bridge): bridge is BridgeERC721 {
+  const cand = bridge as BridgeERC721
+  return cand.supportsERC721 !== undefined && cand.estimateERC721 !== undefined && cand.moveERC721 !== undefined && cand.completeERC721 !== undefined
+}
+
+export interface BridgeERC1155 extends Bridge {
+  supportsERC1155(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<boolean>
+  estimateERC1155(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[]): Promise<MoveEstimate | undefined>
+  moveERC1155(from: NetworkConfig, to: NetworkConfig, token: string, ids: BigNumberish[], amounts: BigNumberish[]): Promise<providers.TransactionRequest[] | undefined>
+  completeERC1155(from: NetworkConfig, to: NetworkConfig, txHash: string, wallet: string): Promise<providers.TransactionRequest[] | undefined>
+}
+
+export function isBridgeERC1155(bridge: Bridge): bridge is BridgeERC1155 {
+  const cand = bridge as BridgeERC1155
+  return cand.supportsERC1155 !== undefined && cand.estimateERC1155 !== undefined && cand.moveERC1155 !== undefined && cand.completeERC1155 !== undefined
 }
--- packages/bridge/src/index.ts
+++ packages/bridge/src/index.ts
@@ -1,6 +1,27 @@
+export {
+  Bridge,
+  BridgeNative,
+  BridgeERC20,
+  BridgeERC721,
+  BridgeERC1155,
+  Move,
+  MoveERC20,
+  MoveERC721,
+  MoveERC1155,
+  isNativeMove,
+  isERC20Move,
+  isERC721Move,
+  isERC1155Move,
+  isBridgeNative,
+  isBridgeERC20,
+  isBridgeERC712,
+  isBridgeERC1155,
+  MoveEstimate
+} from './bridges'
 
-export { Bridge } from './bridges'
+export { MaticPosBridge } from './bridges/matic-bridge'
 
-export { MaticBridge } from './bridges/matic-bridge'
-
-export { BridgesClient } from './client'
+export {
+  BridgesClient,
+  BridgeOption
+} from './client'
