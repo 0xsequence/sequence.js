c33bbe0a77d6dcab125b89baf0651469afc04ae7
Peter Kieltyka <peter.kieltyka@gmail.com>
2021-03-02 07:36:22 -0500

    make 712 use more clear as message digest

--- packages/utils/src/typed-data.ts
+++ packages/utils/src/typed-data.ts
@@ -10,18 +10,16 @@ export interface TypedData {
 
 export type { TypedDataDomain, TypedDataField }
 
-export const encodeTypedDataHash = (typedData: TypedData) => {
+export const encodeTypedDataHash = (typedData: TypedData): string => {
   const types = { ...typedData.types }
   
   // remove EIP712Domain key from types as ethers will auto-gen it in
   // the hash encoder below
   delete types['EIP712Domain']
 
-  return ethers.utils.arrayify(ethers.utils._TypedDataEncoder.hash(typedData.domain, types, typedData.message))
+  return ethers.utils._TypedDataEncoder.hash(typedData.domain, types, typedData.message)
 }
 
-export const encodeTypedDataDigest = (typedData: TypedData) => {
-  const hash = encodeTypedDataHash(typedData)
-  const digest = ethers.utils.arrayify(ethers.utils.keccak256(hash))
-  return digest
+export const encodeTypedDataDigest = (typedData: TypedData): Uint8Array => {
+  return ethers.utils.arrayify(encodeTypedDataHash(typedData))
 }
