bf1111fbe9c3edd094f6a977785e4041b4519500
Agustin Aguilar <Agusxrun@gmail.com>
2021-03-04 15:16:20 +0000

    Include EIP2126 sufix on Guard remote signer response

--- packages/wallet/src/remote-signers/guard-remote-signer.ts
+++ packages/wallet/src/remote-signers/guard-remote-signer.ts
@@ -9,7 +9,7 @@ export class GuardRemoteSigner extends RemoteSigner {
   private readonly _guardd: GuarddService
   private readonly _address: string
 
-  constructor(address: string, hostname: string) {
+  constructor(address: string, hostname: string, public isSequence: boolean = false) {
     super()
     this._guardd = new GuarddService(hostname, fetchPonyfill().fetch)
     this._address = address
@@ -18,7 +18,10 @@ export class GuardRemoteSigner extends RemoteSigner {
   async signMessageWithData(message: BytesLike, auxData?: BytesLike): Promise<string> {
     const request = { msg: ethers.utils.hexlify(message), auxData: ethers.utils.hexlify(auxData ? auxData : []) }
     const res = await this._guardd.sign({ request: request })
-    return res.sig
+
+    // TODO: The guardd service doesn't include the EIP2126 signature type on it's reponse
+    // maybe it should be more explicit and include it? the EIP2126 is only required for non-sequence signatures
+    return this.isSequence ? res.sig : res.sig + '02'
   }
 
   async getAddress(): Promise<string> {
