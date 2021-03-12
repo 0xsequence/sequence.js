a94a6d068e9a01fce0a1a49dcdefdd5d4c04dd8b
Agustin Aguilar <agusxrun@gmail.com>
2021-03-12 17:48:18 +0100

    Fix loading config from intermediary state on require utils (#114)
    * Add tests for sequence utils finder
    
    * Fix loading config from intermediary state on require utils

--- packages/config/src/finder/sequence-utils-finder.ts
+++ packages/config/src/finder/sequence-utils-finder.ts
@@ -56,7 +56,7 @@ export class SequenceUtilsFinder implements ConfigFinder {
     if (requireIndex && logBlockHeight === 0) return { config: undefined }
 
     const filter = authContract.filters.RequiredConfig(address)
-    const lastLog = await this.findLatestLog(this.authProvider, { ...filter, fromBlock: logBlockHeight, toBlock: logBlockHeight !== 0 ? logBlockHeight : 'latest'})
+    const lastLog = await this.findFirstLog(this.authProvider, { ...filter, fromBlock: logBlockHeight, toBlock: logBlockHeight !== 0 ? logBlockHeight : 'latest'})
     if (lastLog === undefined) { console.warn("publishConfig: wallet config last log not found"); return { config: undefined } }
     const event = authContract.interface.decodeEventLog('RequiredConfig', lastLog.data, lastLog.topics)
 
@@ -77,8 +77,36 @@ export class SequenceUtilsFinder implements ConfigFinder {
       }))
     }
 
-    const isValid = currentImplementation === context.mainModuleUpgradable ? imageHash(config) === currentImageHash[0] : addressOf(config, context) === address
+    const isValid = currentImplementation === context.mainModuleUpgradable ? imageHash(config) === currentImageHash[0] : addressOf({ ...config, address: undefined }, context) === address
     if (!isValid) {
+      // Try to find the config for the known image-hash
+      const filter = authContract.filters.RequiredConfig(address, currentImageHash[0])
+      const log = await this.findFirstLog(this.authProvider, { ...filter, fromBlock: logBlockHeight, toBlock: logBlockHeight !== 0 ? logBlockHeight : 'latest'})
+      if (log !== undefined) {
+        const event = authContract.interface.decodeEventLog('RequiredConfig', log.data, log.topics)
+        const signers = ethers.utils.defaultAbiCoder.decode(
+          [`tuple(
+            uint256 weight,
+            address signer
+          )[]`], event._signers
+        )[0]
+
+        const config = {
+          chainId: chainId,
+          address: address,
+          threshold: ethers.BigNumber.from(event._threshold).toNumber(),
+          signers: signers.map((s: any) => ({
+            address: s.signer,
+            weight: ethers.BigNumber.from(s.weight).toNumber()
+          }))
+        }
+
+        if (imageHash(config) === currentImageHash[0]) {
+          return { config }
+        }
+      }
+
+      // If imageHash couldn't be found, return undefined or try without index
       if (ignoreIndex || requireIndex) {
         console.warn('No valid configuration found')
         return { config: undefined }
@@ -124,6 +152,22 @@ export class SequenceUtilsFinder implements ConfigFinder {
       const nhalf = await this.findLatestLog(provider, { ...filter, fromBlock: pivot, toBlock: toBlock })
       if (nhalf !== undefined) return nhalf
       return this.findLatestLog(provider, { ...filter, fromBlock: fromBlock, toBlock: pivot })
+    }
+  }
+
+  private findFirstLog = async (provider: ethers.providers.Provider, filter: ethers.providers.Filter): Promise<ethers.providers.Log | undefined> => {
+    const toBlock = filter.toBlock === 'latest' ? await provider.getBlockNumber() : filter.toBlock as number
+    const fromBlock = filter.fromBlock as number
+
+    try {
+      const logs = await provider.getLogs({ ...filter, toBlock: toBlock })
+      return logs.length === 0 ? undefined : logs[0]
+    } catch (e) {
+      // TODO Don't assume all errors are bad
+      const pivot = Math.floor(((toBlock - fromBlock) / 2) + fromBlock)
+      const nhalf = await this.findFirstLog(provider, { ...filter, fromBlock: fromBlock, toBlock: pivot })
+      if (nhalf !== undefined) return nhalf
+      return this.findFirstLog(provider, { ...filter, fromBlock: pivot, toBlock: toBlock })
     }
   }
 }
