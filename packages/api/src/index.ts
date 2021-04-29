ea71613a9464a26a19bc13d1172cbf32fdc2087a
William Hua <william@attente.ca>
2021-04-29 13:02:11 -0400

    auth: piggyback on already pending JWT and signing requests
    If a session is already in the process of obtaining a JWT or
    authentication signature, reuse the existing requests to avoid spamming
    the signers and servers with multiple identical requests.
    
    Fixes https://github.com/horizon-games/issue-tracker/issues/4689.
    
    Co-authored-by: Ari Lotter <arilotter@gmail.com>
    Co-authored-by: Michael Yu <1619025+acrylix@users.noreply.github.com>

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,35 +1,73 @@
 export * from './api.gen'
 
-import { ArcadeumAPI as BaseArcadeumAPI } from './api.gen'
 import fetch from 'cross-fetch'
 
-export class ArcadeumAPIClient extends BaseArcadeumAPI {
-  jwtAuth: string | undefined = undefined
+import { ArcadeumAPI as BaseArcadeumAPI } from './api.gen'
 
-  constructor(hostname: string) {
+export type RefreshJWTCallback = () => Promise<string>
+
+export class ArcadeumAPIClient extends BaseArcadeumAPI {
+  constructor(
+    hostname: string,
+    private jwtAuth?: string,
+    private refreshJWT?: RefreshJWTCallback
+  ) {
     super(hostname, fetch)
     this.fetch = this._fetch
   }
 
-  _fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
-    return new Promise<Response>((resolve, reject) => {
-      // automatically include jwt auth header to requests
-      // if its been set on the api client
-      const headers: { [key: string]: any } = {}
-      if (this.jwtAuth && this.jwtAuth.length > 0) {
-        headers['Authorization'] = `BEARER ${this.jwtAuth}`
+  _fetch = async (input: RequestInfo, init?: RequestInit, retry: boolean = true): Promise<Response> => {
+    // automatically include jwt auth header to requests
+    // if its been set on the api client
+    const headers: { [key: string]: any } = {}
+    if (this.jwtAuth && this.jwtAuth.length > 0) {
+      headers['Authorization'] = `BEARER ${this.jwtAuth}`
+    }
+
+    // before the request is made
+    init!.headers = { ...init!.headers, ...headers }
+
+    // we should avoid retrying if the request is to /GetAuthToken itself
+    let url: string
+    switch (typeof input) {
+      case 'string':
+        url = input
+        break
+      case 'object':
+        url = input.url
+        break
+    }
+    let isGetAuthToken: boolean
+    try {
+      isGetAuthToken = new URL(url).pathname.endsWith('/GetAuthToken')
+    } catch {
+      isGetAuthToken = url.includes('/GetAuthToken')
+    }
+
+    try {
+      const resp = await fetch(input, init)
+
+      if (resp.status >= 400 && resp.status < 600) {
+        if (retry && this.refreshJWT && !isGetAuthToken) {
+          // refresh token...
+          this.jwtAuth = await this.refreshJWT()
+
+          // ...and try again
+          return this._fetch(input, init, false)
+        }
       }
 
-      // before the request is made
-      init!.headers = { ...init!.headers, ...headers }
-  
-      fetch(input, init).then(resp => {
-        // after the request has been made..
-        resolve(resp)
-      }).catch(err => {
-        // request error
-        reject(err)
-      })
-    })
-  } 
+      return resp
+    } catch (err) {
+      if (retry && this.refreshJWT && !isGetAuthToken) {
+        // refresh token...
+        this.jwtAuth = await this.refreshJWT()
+
+        // ...and try again
+        return this._fetch(input, init, false)
+      }
+
+      throw err
+    }
+  }
 }
