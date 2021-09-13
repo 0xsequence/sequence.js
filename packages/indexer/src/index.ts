940cd7ae98b629fac1080cc40f6c9bd3f18b1910
William Hua <william@attente.ca>
2021-09-13 15:10:20 -0400

    auth, indexer, metadata, network: single API instance
    Main changes to Sequence architecture:
    
    - only one API instance, API is no longer a per-chain service
    - separate per-chain indexer service, API no longer handles indexing
    - single contract metadata service, API no longer serves metadata
    
    Co-authored-by: Corban Riley <corbanbrook@gmail.com>

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -1,1 +1,26 @@
 export * from './indexer.gen'
+
+import fetch from 'cross-fetch'
+
+import { Indexer as BaseSequenceIndexer } from './indexer.gen'
+
+export class SequenceIndexerClient extends BaseSequenceIndexer {
+  constructor(hostname: string, public jwtAuth?: string) {
+    super(hostname, fetch)
+    this.fetch = this._fetch
+  }
+
+  _fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
+    // automatically include jwt auth header to requests
+    // if its been set on the api client
+    const headers: { [key: string]: any } = {}
+    if (this.jwtAuth && this.jwtAuth.length > 0) {
+      headers['Authorization'] = `BEARER ${this.jwtAuth}`
+    }
+
+    // before the request is made
+    init!.headers = { ...init!.headers, ...headers }
+
+    return fetch(input, init)
+  }
+}
--- packages/metadata/src/index.ts
+++ packages/metadata/src/index.ts
@@ -1,1 +1,11 @@
 export * from './metadata.gen'
+
+import fetch from 'cross-fetch'
+
+import { Metadata as BaseSequenceMetadata } from './metadata.gen'
+
+export class SequenceMetadataClient extends BaseSequenceMetadata {
+  constructor(hostname: string) {
+    super(hostname, fetch)
+  }
+}
