1652a7a60cf546860730e050f0ff34fd3441ef13
Michael Yu <1619025+acrylix@users.noreply.github.com>
2021-11-21 20:38:34 -0500

    remove use of ora for deployer

--- packages/deployer/src/utils/logger.ts
+++ packages/deployer/src/utils/logger.ts
@@ -1,5 +1,3 @@
-import { isNode } from '@0xsequence/utils'
-
 export interface Logger {
   start(text?: string): void
   stop(): void
@@ -9,38 +7,28 @@ export interface Logger {
   info(text?: string): void
 }
 
-const loadOra = () => import('ora')
-
 export const createLogger = async (): Promise<Logger> => {
-  if (isNode()) {
-    // dynamically import ora package only for nodejs
-    return (await loadOra()).default({ discardStdin: true })
-  } else {
-    // returns a noop ora-compatible logger
-    const debug = false
-    let startText = ''
-    return {
-      start: function (text: string = '') {
-        startText = text
-        if (debug) console.log(`start ${text}`)
-      },
-      stop: function () {
-        if (debug) console.log(`stop ${startText}`)
-        startText = ''
-      },
-      succeed: function (text: string = '') {
-        if (debug) console.log(`success ${startText} ${text}`)
-      },
-      fail: function (text: string = '') {
-        if (debug) console.log(`fail ${startText} ${text}`)
-        throw `fail: ${startText} ${text}`
-      },
-      warn: function (text: string = '') {
-        if (debug) console.log(`warn ${startText} ${text}`)
-      },
-      info: function (text: string = '') {
-        if (debug) console.log(`info ${startText} ${text}`)
-      }
+  let startText = ''
+  return {
+    start: function (text: string = '') {
+      startText = text
+      console.warn(`[start] ${text}`)
+    },
+    stop: function () {
+      console.warn(`[stop] ${startText}`)
+      startText = ''
+    },
+    succeed: function (text: string = '') {
+      console.warn(`[success] ${startText} ${text}`)
+    },
+    fail: function (text: string = '') {
+      console.warn(`[fail] ${startText} ${text}`)
+    },
+    warn: function (text: string = '') {
+      console.warn(`[warn] ${startText} ${text}`)
+    },
+    info: function (text: string = '') {
+      console.warn(`[info] ${startText} ${text}`)
     }
   }
 }
