e22fd451efc4a9b319442d9234367eb6a7a1c115
Corban Riley <corbanbrook@gmail.com>
2022-04-19 12:53:09 -0400

    Adding networks const to networks config

--- packages/network/src/config.ts
+++ packages/network/src/config.ts
@@ -44,9 +44,11 @@ export enum ChainId {
 export interface NetworkConfig {
   title?: string
   name: string
-  chainId: number
+  chainId: ChainId
   ensAddress?: string
   testnet?: boolean
+
+  blockExplorer?: BlockExplorerConfig
 
   rpcUrl?: string
   provider?: JsonRpcProvider
@@ -66,6 +68,188 @@ export interface NetworkConfig {
   isAuthChain?: boolean
 }
 
+export type BlockExplorerConfig = {
+  name?: string
+  rootUrl: string
+  addressUrl?: string
+  txnHashUrl?: string
+}
+
+export const networks: Record<ChainId, NetworkConfig> = {
+  [ChainId.MAINNET]: {
+    chainId: ChainId.MAINNET,
+    name: 'mainnet',
+    title: 'Ethereum',
+    blockExplorer: {
+      name: 'Etherscan',
+      rootUrl: 'https://etherscan.io/'
+    }
+  },
+  [ChainId.ROPSTEN]: {
+    chainId: ChainId.ROPSTEN,
+    name: 'ropsten',
+    title: 'Ropsten',
+    testnet: true,
+    blockExplorer: {
+      name: 'Etherscan (Ropsten)',
+      rootUrl: 'https://ropsten.etherscan.io/'
+    }
+  },
+  [ChainId.RINKEBY]: {
+    chainId: ChainId.RINKEBY,
+    name: 'rinkeby',
+    title: 'Rinkeby',
+    testnet: true,
+    blockExplorer: {
+      name: 'Etherscan (Rinkeby)',
+      rootUrl: 'https://rinkeby.etherscan.io/'
+    }
+  },
+  [ChainId.GOERLI]: {
+    chainId: ChainId.GOERLI,
+    name: 'goerli',
+    title: 'Goerli',
+    testnet: true,
+    blockExplorer: {
+      name: 'Etherscan (Goerli)',
+      rootUrl: 'https://goerli.etherscan.io/'
+    }
+  },
+  [ChainId.KOVAN]: {
+    chainId: ChainId.KOVAN,
+    name: 'kovan',
+    title: 'Kovan',
+    testnet: true,
+    blockExplorer: {
+      name: 'Etherscan (Kovan)',
+      rootUrl: 'https://kovan.etherscan.io/'
+    }
+  },
+  [ChainId.POLYGON]: {
+    chainId: ChainId.POLYGON,
+    name: 'polygon',
+    title: 'Polygon',
+    blockExplorer: {
+      name: 'Polygonscan',
+      rootUrl: 'https://polygonscan.com/'
+    }
+  },
+  [ChainId.POLYGON_MUMBAI]: {
+    chainId: ChainId.POLYGON_MUMBAI,
+    name: 'mumbai',
+    title: 'Polygon Mumbai',
+    testnet: true,
+    blockExplorer: {
+      name: 'Polygonscan (Mumbai)',
+      rootUrl: 'https://mumbai.polygonscan.com/'
+    }
+  },
+  [ChainId.BSC]: {
+    chainId: ChainId.BSC,
+    name: 'bsc',
+    title: 'BNB Smart Chain',
+    blockExplorer: {
+      name: 'BSCScan',
+      rootUrl: 'https://bscscan.com/'
+    }
+  },
+  [ChainId.BSC_TESTNET]: {
+    chainId: ChainId.BSC_TESTNET,
+    name: 'bsc-testnet',
+    title: 'BSC Testnet',
+    testnet: true,
+    blockExplorer: {
+      name: 'BSCScan (Testnet)',
+      rootUrl: 'https://testnet.bscscan.com/'
+    }
+  },
+  [ChainId.OPTIMISM]: {
+    chainId: ChainId.OPTIMISM,
+    name: 'optimism',
+    title: 'Optimism',
+    blockExplorer: {
+      name: 'Etherscan (Optimism)',
+      rootUrl: 'https://optimistic.etherscan.io/'
+    }
+  },
+  [ChainId.OPTIMISM_TESTNET]: {
+    chainId: ChainId.OPTIMISM_TESTNET,
+    name: 'optimism-testnet',
+    title: 'Optimistic Kovan',
+    testnet: true,
+    blockExplorer: {
+      name: 'Etherscan (Optimism Testnet)',
+      rootUrl: 'https://kovan-optimistic.etherscan.io/'
+    }
+  },
+  [ChainId.ARBITRUM]: {
+    chainId: ChainId.ARBITRUM,
+    name: 'arbitrum',
+    title: 'Arbitrum',
+    blockExplorer: {
+      name: 'Arbiscan',
+      rootUrl: 'https://arbiscan.io/'
+    }
+  },
+  [ChainId.ARBITRUM_TESTNET]: {
+    chainId: ChainId.ARBITRUM_TESTNET,
+    name: 'arbitrum-testnet',
+    title: 'Arbitrum Testnet',
+    testnet: true,
+    blockExplorer: {
+      name: 'Arbiscan (Testnet)',
+      rootUrl: 'https://testnet.arbiscan.io/'
+    }
+  },
+  [ChainId.AVALANCHE]: {
+    chainId: ChainId.AVALANCHE,
+    name: 'avalanche',
+    title: 'Avalanche',
+    blockExplorer: {
+      name: 'Snowtrace',
+      rootUrl: 'https://snowtrace.io/'
+    }
+  },
+  [ChainId.AVALANCHE_TESTNET]: {
+    chainId: ChainId.AVALANCHE_TESTNET,
+    name: 'avalanche-testnet',
+    title: 'Avalanche Testnet',
+    testnet: true,
+    blockExplorer: {
+      name: 'Snowtrace (Testnet)',
+      rootUrl: 'https://testnet.snowtrace.io/'
+    }
+  },
+  [ChainId.FANTOM]: {
+    chainId: ChainId.FANTOM,
+    name: 'fantom',
+    title: 'Fantom',
+    blockExplorer: {
+      name: 'FTMScan',
+      rootUrl: 'https://ftmscan.com/'
+    }
+  },
+  [ChainId.FANTOM_TESTNET]: {
+    chainId: ChainId.FANTOM_TESTNET,
+    name: 'fantom-testnet',
+    title: 'Fantom Testnet',
+    testnet: true,
+    blockExplorer: {
+      name: 'FTMScan (Testnet)',
+      rootUrl: 'https://testnet.ftmscan.com/'
+    }
+  },
+  [ChainId.GNOSIS]: {
+    chainId: ChainId.GNOSIS,
+    name: 'gnosis',
+    title: 'Gnosis Chain',
+    blockExplorer: {
+      name: 'Gnosis Chain Explorer',
+      rootUrl: 'https://blockscout.com/xdai/mainnet/'
+    }
+  }
+}
+
 export type Networks = NetworkConfig[]
 
 export type ChainIdLike = NetworkConfig | BigNumberish
@@ -75,18 +259,14 @@ export type NetworksBuilder = (vars: { [key: string]: any }) => Networks
 export const mainnetNetworks = createNetworkConfig(
   (vars: { [key: string]: any }) => [
     {
-      title: 'Ethereum',
-      name: 'mainnet',
-      chainId: ChainId.MAINNET,
+      ...networks[ChainId.MAINNET],
       ensAddress: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
       rpcUrl: urlClean(`${vars.baseRpcUrl}/mainnet`),
       relayer: { url: urlClean(`${vars.baseRelayerUrl}/mainnet`) },
       isDefaultChain: true
     },
     {
-      title: 'Polygon',
-      name: 'polygon',
-      chainId: ChainId.POLYGON,
+      ...networks[ChainId.POLYGON],
       rpcUrl: 'https://rpc-mainnet.matic.network',
       relayer: { url: urlClean(`${vars.baseRelayerUrl}/matic`) },
       isAuthChain: true
@@ -102,42 +282,19 @@ export const mainnetNetworks = createNetworkConfig(
 export const testnetNetworks = createNetworkConfig(
   (vars: { [key: string]: any }) => [
     {
-      name: 'rinkeby',
-      chainId: ChainId.RINKEBY,
+      ...networks[ChainId.RINKEBY],
       ensAddress: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-      testnet: true,
       rpcUrl: urlClean(`${vars.baseRpcUrl}/rinkeby`),
       relayer: { url: urlClean(`${vars.baseRelayerUrl}/rinkeby`) },
       isDefaultChain: true
     },
     {
-      name: 'goerli',
-      chainId: ChainId.GOERLI,
+      ...networks[ChainId.GOERLI],
       ensAddress: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-      testnet: true,
       rpcUrl: urlClean(`${vars.baseRpcUrl}/goerli`),
       relayer: { url: urlClean(`${vars.baseRelayerUrl}/goerli`) },
       isAuthChain: true
     }
-    // {
-    //   name: 'ropsten',
-    //   chainId: ChainId.ROPSTEN,
-    //   ensAddress: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-    //   rpcUrl: '',
-    //   // relayer: null
-    // },
-    // {
-    //   name: 'kovan',
-    //   chainId: ChainId.KOVAN,
-    //   rpcUrl: '',
-    //   relayer: null
-    // },
-    // {
-    //   name: 'mumbai',
-    //   chainId: ChainId.POLYGON_MUMBAI,
-    //   rpcUrl: 'https://rpc-mumbai.matic.today',
-    //   relayer: null
-    // }
   ],
   undefined,
   {
@@ -145,7 +302,3 @@ export const testnetNetworks = createNetworkConfig(
     baseRelayerUrl: 'https://relayers.sequence.app'
   }
 )
-
-// export const mainnetNetworksIndex = networksIndex(mainnetNetworks)
-
-// export const testnetNetworksIndex = networksIndex(testnetNetworks)
