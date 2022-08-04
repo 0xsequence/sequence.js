244762a6b4cfca92ed709064574269578964fe97
tolgahan-arikan <tolgahan.arikan@gmail.com>
2022-08-05 00:59:54 +0300

    prototype: make connect work with finalizeConnect

--- packages/provider/src/transports/url-transport/browser-redirect-hooks.ts
+++ packages/provider/src/transports/url-transport/browser-redirect-hooks.ts
@@ -1,20 +1,23 @@
-import { OpenWalletIntent, ProviderRpcError, ProviderMessageResponse } from '../../types'
+import { OpenWalletIntent, ProviderRpcError, ProviderMessageResponse, ProviderMessage } from '../../types'
 import { UrlMessageProviderHooks } from './url-message-provider'
 
 export class BrowserRedirectMessageHooks implements UrlMessageProviderHooks {
-  openWallet = (walletBaseUrl: string, path?: string, intent?: OpenWalletIntent, networkId?: string | number): void => {
-    console.log('yeayyyyyyyyyyyyy')
+  openWallet = (walletBaseUrl: string): void => {
+    console.log('BrowserRedirectMessageHooks openWallet', walletBaseUrl)
     window.location.href = walletBaseUrl
-    
+
     // on 0xsequence/react-native, we have to implement UrlMessageHooks
     // which will call InAppBrowser.open() etc.........
   }
 
-  openWallet2(walletUrl: string): void {
-
-  }
-
-  async fetchResponseFromRedirectUrl(): Promise<{error?: ProviderRpcError, response?: ProviderMessageResponse}> {
-    return {}
-  }
+  // listenResponseFromRedirectUrl(callback: (response: ProviderMessage<any>) => void): void {
+  //   console.log('listenResponseFromRedirectUrl', window.location.href)
+  //   // callback({
+  //   //   idx: 1,
+  //   //   type: 'something',
+  //   //   data: {},
+  //   //   chainId: undefined,
+  //   //   origin: undefined
+  //   // })
+  // }
 }
--- packages/provider/src/transports/url-transport/url-message-handler.ts
+++ packages/provider/src/transports/url-transport/url-message-handler.ts
@@ -1,56 +1,100 @@
 import { BaseWalletTransport } from '../base-wallet-transport'
 import { WalletRequestHandler } from '../wallet-request-handler'
-import { InitState, ProviderMessage } from '../../types'
-import { base64DecodeObject } from '@0xsequence/utils'
+import {
+  EventType,
+  InitState,
+  OpenWalletIntent,
+  ProviderMessage,
+  ProviderRpcError,
+  TransportSession,
+  WindowSessionParams
+} from '../../types'
+import { base64DecodeObject, base64EncodeObject, logger } from '@0xsequence/utils'
 
 export class UrlMessageHandler extends BaseWalletTransport {
-  
   private _pathname: string
+  private _redirectUrl: string = ''
+  private _messages: ProviderMessage<any>[] = []
+
+  private _lastMessageAt: number = 0
 
   constructor(walletRequestHandler: WalletRequestHandler, pathname: string) {
     super(walletRequestHandler)
-    this._init = InitState.NIL
+    this._init = InitState.OK
     this._pathname = pathname
   }
 
-  register() {
+  async register() {
     const { pathname, search: rawParams } = new URL(window.location.href)
     if (pathname !== this._pathname) {
       return
     }
 
-    console.log('weeeeeeeeeee')
+    console.log('... url message handler register ...')
 
     const params = new URLSearchParams(rawParams)
-    // const request = base64DecodeObject(params.get('request'))
     const redirectUrl = params.get('redirectUrl')
+    const intent = base64DecodeObject(params.get('intent')) as OpenWalletIntent
+    this._redirectUrl = redirectUrl!
+
+    console.log('intent', intent)
 
     // TODO: ensure we have both of these, otherwise just return and skip,
     // maybe though console.warn
 
-    console.log('=====> redirecting to.....', redirectUrl)
-    {
-      (window.location as any).href = redirectUrl
+    let session: TransportSession | null = this.getWindowTransportSession(rawParams)
+
+    // // provider should always include sid when opening a new window
+    const isNewWindowSession = !!session.sessionId
+
+    // // attempt to restore previous session in the case of a redirect or window reload
+    if (!isNewWindowSession) {
+      session = await this.getCachedTransportSession()
     }
 
-    // this.walletRequestHandler.sendMessageRequest(request).then(response => {
-    //   console.log('got the response.......', response)
-    // })
+    if (!session) {
+      logger.error('window session is undefined')
+      return
+    }
 
+    this._registered = true
+    this.open(session).catch(e => {
+      const err = `failed to open to network ${session?.networkId}, due to: ${e}`
+      logger.error(err)
+      this.notifyClose({ message: err } as ProviderRpcError)
+      // redirect?
+      // window.close()
+    })
   }
 
-  unregister() {
-  }
+  unregister() {}
 
   sendMessage(message: ProviderMessage<any>) {
-    // TODO... encode the response and then, call the redirect url..
+    console.log('... sendMessage in url-message-handler ...', message)
 
-    console.log('urlMessageHandler sendMessage', message)
+    this._messages.push(message)
+    this._lastMessageAt = Date.now()
 
-    const redirectUrl = 'etc.etc.'
+    // temporary, just to send back connect so that we don't navigate for INIT etc.
+    setTimeout(() => {
+      if (this._lastMessageAt == 0 || this._lastMessageAt + 1000 > Date.now()) {
+        return
+      } else {
+        this._lastMessageAt = 0
+        const connect = this._messages.find(m => m.type === EventType.CONNECT)
+        const redirectUrl = new URL(this._redirectUrl)
+        redirectUrl.searchParams.set('response', base64EncodeObject(connect))
+        window.location.href = redirectUrl.href
+      }
+    }, 1000)
+  }
 
-    // TODO: check that window.location exists..
-
-    window.location.href = redirectUrl
+  private getWindowTransportSession = (windowParams: string | undefined): TransportSession => {
+    const params = new WindowSessionParams(windowParams)
+    return {
+      sessionId: params.get('sid'),
+      networkId: params.get('net'),
+      intent: base64DecodeObject<OpenWalletIntent>(params.get('intent'))
+    }
   }
 }
