f5a2ad71d0f80ab39cb68b5cef0118fff60b9546
Peter Kieltyka <pkieltyka@users.noreply.github.com>
2022-10-06 08:30:31 -0400

    disable rinkeby network support (#299)

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -6,10 +6,21 @@ import { Indexer as BaseSequenceIndexer } from './indexer.gen'
 
 export enum SequenceIndexerServices {
   MAINNET = 'https://mainnet-indexer.sequence.app',
+
   POLYGON = 'https://polygon-indexer.sequence.app',
-  
-  RINKEBY = 'https://rinkeby-indexer.sequence.app',
-  POLYGON_MUMBAI = 'https://mumbai-indexer.sequence.app'
+  POLYGON_MUMBAI = 'https://mumbai-indexer.sequence.app',
+
+  ARBITRUM = 'https://arbitrum-indexer.sequence.app',
+  ARBITRUM_NOVA = 'https://arbitrum-nova-indexer.sequence.app',
+
+  OPTIMISM = 'https://optimism-indexer.sequence.app',
+  AVALANCHE = 'https://avalanche-indexer.sequence.app',
+  GNOSIS = 'https://gnosis-indexer.sequence.app',
+
+  BSC = 'https://bsc-indexer.sequence.app',
+  BSC_TESTNET = 'https://bsc-testnet-indexer.sequence.app',
+
+  GOERLI = 'https://goerli-indexer.sequence.app'
 }
 
 export class SequenceIndexerClient extends BaseSequenceIndexer {
@@ -32,3 +43,16 @@ export class SequenceIndexerClient extends BaseSequenceIndexer {
     return fetch(input, init)
   }
 }
+
+
+// const SequenceIndexerServices: { [key: string]: string } = {}
+
+// {
+//   [ ...mainnetNetworks, ...testnetNetworks ].forEach(n => {
+//     if (n.indexerUrl) {
+//       SequenceIndexerServices[n.name.toUpperCase()] = n.indexerUrl
+//     }
+//   })
+// }
+
+// export { SequenceIndexerServices }
