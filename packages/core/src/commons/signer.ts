9deef748b7dc540ddc0b63f5fa9c70a716ca44fd
Agustin Aguilar <agusgit@pm.me>
2022-11-04 18:52:41 +0000

    Add signature decode methods

--- packages/core/src/commons/signer.ts
+++ packages/core/src/commons/signer.ts
@@ -1,4 +1,5 @@
 import { ethers } from "ethers"
+import { isValidEIP1271Signature } from "./validateEIP1271"
 
 export enum SigType {
   EIP712 = 1,
@@ -25,6 +26,28 @@ export function recoverSigner(digest: ethers.BytesLike, signature: ethers.BytesL
 
   if (type === SigType.ETH_SIGN) {
     return ethers.utils.recoverAddress(ethers.utils.hashMessage(digest), splitSignature)
+  }
+
+  throw new Error(`Unsupported signature type: ${type}`)
+}
+
+export function isValidSignature(
+  address: string,
+  digest: string,
+  signature: ethers.BytesLike,
+  provider: ethers.providers.Provider
+) {
+  const bytes = ethers.utils.arrayify(signature)
+
+  // type is last byte
+  const type = bytes[bytes.length - 1]
+
+  if (type === SigType.EIP712 || type === SigType.ETH_SIGN) {
+    return address === recoverSigner(digest, signature)
+  }
+
+  if (type === SigType.WALLET_BYTES32) {
+    return isValidEIP1271Signature(address, digest, bytes.slice(0, -1), provider)
   }
 
   throw new Error(`Unsupported signature type: ${type}`)
