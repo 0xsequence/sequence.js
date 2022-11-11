ce5d8c4c34ae76ca51b3cec4588b9907fca6bb76
Agustin Aguilar <agusgit@pm.me>
2022-11-11 20:31:42 +0000

    Add signing orchestrator

---
+++ packages/signhub/src/signers/wrapper.ts
@@ -0,0 +1,27 @@
+
+import { ethers } from 'ethers'
+import { Status } from '../orchestrator'
+import { SapientSigner } from './signer'
+
+export class SignerWrapper implements SapientSigner {
+  constructor(public signer: ethers.Signer, public eoa: boolean = true) {}
+
+  getAddress(): Promise<string> {
+    return this.signer.getAddress()
+  }
+
+  async requestSignature(message: ethers.utils.BytesLike, callbacks: {
+    onSignature: (signature: ethers.utils.BytesLike) => void;
+    onRejection: (error: string) => void;
+    onStatus: (situation: string) => void
+  }): Promise<boolean> {
+    callbacks.onSignature(await this.signer.signMessage(message))    
+    return true
+  }
+
+  notifyStatusChange(_: Status): void {}
+
+  isEOA(): boolean {
+    return this.eoa
+  }
+}
