0b33da54d9972989c8dbbd875809483c1c88e4d2
Agustin Aguilar <agusgit@pm.me>
2022-11-18 17:25:10 +0000

    Initial commit migration framework

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 
+export * as v1 from './v1'
 export * as v2 from "./v2"
 export * as commons from "./commons"
---
+++ packages/migration/src/migrations/index.ts
@@ -0,0 +1,28 @@
+import { commons } from "@0xsequence/core"
+import { VersionedContext } from "../context"
+
+//                                 = uint160(keccak256("org.sequence.sdk.migration.space.nonce"))
+export const MIGRATION_NONCE_SPACE = "0xa04263acf755e8bd19c0d7e20eea39a9ff3729eb"
+
+export interface Migration<
+  P extends commons.config.Config,
+  C extends commons.config.Config,
+> {
+  version: number,
+
+  buildTransaction: (
+    address: string,
+    contexts: VersionedContext,
+    newConfig: P | C
+  ) => commons.transaction.TransactionBundle
+
+  decodeTransaction: (
+    tx: commons.transaction.TransactionBundle,
+    contexts: VersionedContext
+  ) => {
+    address: string,
+    newConfig: C
+  }
+}
+
+export * as v1v2 from './migration_01_02'
