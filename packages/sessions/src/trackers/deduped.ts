90e5bc708e708c05441ba55fc9313b873a4edcd4
William Hua <william@attente.ca>
2023-03-08 18:34:47 -0500

    utils: promise cache

--- packages/sessions/src/trackers/deduped.ts
+++ packages/sessions/src/trackers/deduped.ts
@@ -1,13 +1,14 @@
 import { commons } from "@0xsequence/core"
 import { migrator } from "@0xsequence/migration";
-import { BigNumber, BigNumberish, ethers } from "ethers";
+import { BigNumber, BigNumberish } from "ethers";
 import { ConfigTracker, PresignedConfig, PresignedConfigLink } from "../tracker";
+import { PromiseCache } from "./promise-cache";
 
 // This tracks wraps another tracker and dedupes calls to it, so in any calls
 // are sent in short succession, only the first call is forwarded to the
 // underlying tracker, and the rest are ignored.
 export class DedupedTracker implements migrator.PresignedMigrationTracker, ConfigTracker {
-  private readonly pending: Map<string, { promise: Promise<any>, time: number }> = new Map();
+  private readonly cache: PromiseCache = new PromiseCache();
 
   constructor(
     private readonly tracker: migrator.PresignedMigrationTracker & ConfigTracker,
@@ -15,73 +16,43 @@ export class DedupedTracker implements migrator.PresignedMigrationTracker, Confi
     public verbose = false
   ) {}
 
-  async dedupe<T, Y extends Array<any>>(key: string, fn: (...args: Y) => Promise<T>, ...args: Y): Promise<T> {
-    this.clear()
-
-    // TODO: Replace with a faster hash function
-    const subkey = `${key}:${ethers.utils.keccak256(ethers.utils.toUtf8Bytes(JSON.stringify(args)))}`
-    const now = Date.now()
-    const pending = this.pending.get(subkey)
-
-    if (pending && now - pending.time < this.window) {
-      if (this.verbose) {
-        console.log(`dedupe hit: ${subkey} -> found (${pending.time})`)
-      }
-      return pending.promise as Promise<T>
-    }
-
-    const promise = fn(...args)
-    this.pending.set(subkey, { promise, time: now })
-    return promise
-  }
-
-  clear() {
-    // remove all pending calls past the window
-    const now = Date.now()
-    for (const [key, pending] of this.pending) {
-      if (now - pending.time > this.window) {
-        this.pending.delete(key)
-      }
-    }
-  }
-
   configOfImageHash(args: { imageHash: string; }): Promise<commons.config.Config | undefined> {
-    return this.dedupe('configOfImageHash', (args) => this.tracker.configOfImageHash(args), args)
+    return this.cache.do('configOfImageHash', this.window, args => this.tracker.configOfImageHash(args), args)
   }
 
   getMigration(address: string, fromImageHash: string, fromVersion: number, chainId: BigNumberish): Promise<migrator.SignedMigration | undefined> {
-    return this.dedupe('getMigration', (...args) => this.tracker.getMigration(...args), address, fromImageHash, fromVersion, chainId)
+    return this.cache.do('getMigration', this.window, (...args) => this.tracker.getMigration(...args), address, fromImageHash, fromVersion, chainId)
   }
 
   saveMigration(address: string, signed: migrator.SignedMigration, contexts: commons.context.VersionedContext): Promise<void> {
-    return this.dedupe('saveMigration', (...args) => this.tracker.saveMigration(...args), address, signed, contexts)
+    return this.cache.do('saveMigration', undefined, (...args) => this.tracker.saveMigration(...args), address, signed, contexts)
   }
 
   loadPresignedConfiguration(args: { wallet: string; fromImageHash: string; longestPath?: boolean | undefined; }): Promise<PresignedConfigLink[]> {
-    return this.dedupe('loadPresignedConfiguration', (args) => this.tracker.loadPresignedConfiguration(args), args)
+    return this.cache.do('loadPresignedConfiguration', this.window, args => this.tracker.loadPresignedConfiguration(args), args)
   }
 
   savePresignedConfiguration(args: PresignedConfig): Promise<void> {
-    return this.dedupe('savePresignedConfiguration', (args) => this.tracker.savePresignedConfiguration(args), args)
+    return this.cache.do('savePresignedConfiguration', undefined, args => this.tracker.savePresignedConfiguration(args), args)
   }
 
   saveWitnesses(args: { wallet: string; digest: string; chainId: BigNumberish; signatures: string[]; }): Promise<void> {
-    return this.dedupe('saveWitnesses', (args) => this.tracker.saveWitnesses(args), args)
+    return this.cache.do('saveWitnesses', undefined, args => this.tracker.saveWitnesses(args), args)
   }
 
   saveWalletConfig(args: { config: commons.config.Config; }): Promise<void> {
-    return this.dedupe('saveWalletConfig', (args) => this.tracker.saveWalletConfig(args), args)
+    return this.cache.do('saveWalletConfig', undefined, args => this.tracker.saveWalletConfig(args), args)
   }
 
   imageHashOfCounterfactualWallet(args: { wallet: string; }): Promise<{ imageHash: string; context: commons.context.WalletContext; } | undefined> {
-    return this.dedupe('imageHashOfCounterfactualWallet', (args) => this.tracker.imageHashOfCounterfactualWallet(args), args)
+    return this.cache.do('imageHashOfCounterfactualWallet', undefined, args => this.tracker.imageHashOfCounterfactualWallet(args), args)
   }
 
   saveCounterfactualWallet(args: { config: commons.config.Config; context: commons.context.WalletContext[]; }): Promise<void> {
-    return this.dedupe('saveCounterfactualWallet', (args) => this.tracker.saveCounterfactualWallet(args), args)
+    return this.cache.do('saveCounterfactualWallet', undefined, args => this.tracker.saveCounterfactualWallet(args), args)
   }
 
   walletsOfSigner(args: { signer: string; }): Promise<{ wallet: string; proof: { digest: string; chainId: BigNumber; signature: string; }; }[]> {
-    return this.dedupe('walletsOfSigner', (args) => this.tracker.walletsOfSigner(args), args)
+    return this.cache.do('walletsOfSigner', this.window, args => this.tracker.walletsOfSigner(args), args)
   }
 }
---
+++ packages/utils/src/promise-cache.ts
@@ -0,0 +1,51 @@
+import { ethers } from 'ethers'
+
+export class PromiseCache {
+  private readonly cache: Map<string, Entry>
+
+  constructor() {
+    this.cache = new Map()
+  }
+
+  do<S extends Array<unknown>, T>(
+    key: string,
+    validMilliseconds: number | undefined,
+    task: (...args: S) => Promise<T>,
+    ...args: S
+  ): Promise<T> {
+    key = `${key}:${ethers.utils.keccak256(ethers.utils.toUtf8Bytes(JSON.stringify(args)))}`
+
+    let entry = this.cache.get(key)
+
+    if (entry) {
+      if (entry.expiration) {
+        if (new Date() >= entry.expiration) {
+          entry = undefined
+          this.cache.delete(key)
+        }
+      }
+    }
+
+    if (!entry) {
+      entry = { promise: Promise.reject('unreachable') }
+      if (validMilliseconds === undefined) {
+        entry.promise = task(...args)
+      } else {
+        entry.promise = task(...args).then(result => {
+          if (entry) {
+            entry.expiration = new Date(Date.now() + validMilliseconds)
+          }
+          return result
+        })
+      }
+      this.cache.set(key, entry)
+    }
+
+    return entry.promise as Promise<T>
+  }
+}
+
+type Entry = {
+  promise: Promise<unknown>
+  expiration?: Date
+}
