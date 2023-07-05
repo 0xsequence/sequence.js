908005e476e9e52b4204562dba1d733565dc2856
Agustin Aguilar <agusgit@pm.me>
2023-07-05 15:49:12 +0000

    Use EIP-6492 for client side validation testing

--- packages/auth/src/proof.ts
+++ packages/auth/src/proof.ts
@@ -4,46 +4,17 @@ import { tracker } from "@0xsequence/sessions"
 import { ethers } from "ethers"
 
 export const ValidateSequenceWalletProof = (
-  reader: commons.reader.Reader,
+  readerFor: (chainId: number) => commons.reader.Reader,
   tracker: tracker.ConfigTracker,
-  context: commons.context.WalletContext,
-  coders: {
-    signature: commons.signature.SignatureCoder,
-    config: commons.config.ConfigCoder,
-  }
+  context: commons.context.WalletContext
 ): ValidatorFunc => {
   return async (
-    provider: ethers.providers.JsonRpcProvider,
+    _provider: ethers.providers.JsonRpcProvider,
     chainId: number,
     proof: Proof
   ): Promise<{ isValid: boolean }> => {
     const digest = proof.messageDigest()
-    const isDeployed = await reader.isDeployed(proof.address)
-
-    if (isDeployed) {
-      // Easy, we just call the contract to validate the signature
-      const isValid = await reader.isValidSignature(proof.address, digest, proof.signature)
-      return { isValid }
-    }
-
-    // We need to fully recover the signature, compute the config
-    // then the imageHash, and finally check if the imageHash matches
-    // the counterfactual address.
-
-    // NOTICE: We should replace this by an EIP used to validate undeployed wallet's signatures
-
-    const decoded = coders.signature.decode(proof.signature)
-    const recovered = await coders.signature.recover(decoded, {
-      address: proof.address,
-      digest: ethers.utils.hexlify(digest),
-      chainId
-    }, provider)
-
-    const imageHash = coders.config.imageHashOf(recovered.config)
-    const counterfactualAddress = commons.context.addressOf(context, imageHash)
-
-    return {
-      isValid: counterfactualAddress.toLowerCase() === proof.address.toLowerCase()
-    }
+    const isValid = await readerFor(chainId).isValidSignature(proof.address, digest, proof.signature)
+    return { isValid }
   }
 }
