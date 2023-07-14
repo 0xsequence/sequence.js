a00335a9255f5d214ed157c2521f6b58d9970593
Agusx1211 <agusgit@pm.me>
2023-07-14 06:00:46 +0000

    Add walletCreationCode to deployed contexts

--- packages/core/src/v2/index.ts
+++ packages/core/src/v2/index.ts
@@ -21,5 +21,5 @@ export const DeployedWalletContext: WalletContext = {
   guestModule: '0xfea230Ee243f88BC698dD8f1aE93F8301B6cdfaE',
   mainModule: '0xfBf8f1A5E00034762D928f46d438B947f5d4065d',
   mainModuleUpgradable: '0x4222dcA3974E39A8b41c411FeDDE9b09Ae14b911',
-  walletCreationCode: '',
+  walletCreationCode: '0x603a600e3d39601a805130553df3363d3d373d3d3d363d30545af43d82803e903d91601857fd5bf3',
 }
