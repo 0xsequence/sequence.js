0e8afb240ed391d5c3b223f422df685d51d219f0
William Hua <william@attente.ca>
2023-10-10 09:05:49 -0400

    guard: auth methods interface

--- packages/guard/src/index.ts
+++ packages/guard/src/index.ts
@@ -1,2 +1,1 @@
-export * from './guard.gen'
 export * from './signer'
--- packages/guard/src/signer.ts
+++ packages/guard/src/signer.ts
@@ -1,7 +1,9 @@
-import { signers, Status } from '@0xsequence/signhub'
-import { BytesLike, ethers } from 'ethers'
-import { Guard } from './guard.gen'
+import { Account } from '@0xsequence/account'
 import { commons, universal } from '@0xsequence/core'
+import { signers, Status } from '@0xsequence/signhub'
+import { encodeTypedDataDigest, TypedData } from '@0xsequence/utils'
+import { BytesLike, ethers, TypedDataDomain } from 'ethers'
+import { AuthMethodsReturn, Guard, RecoveryCode as GuardRecoveryCode } from './guard.gen'
 
 const fetch = typeof global === 'object' ? global.fetch : window.fetch
 
@@ -77,6 +79,112 @@ export class GuardSigner implements signers.SapientSigner {
     this.evaluateRequest(id, status.message, status, metadata)
   }
 
+  async getAuthMethods(proof: OwnershipProof): Promise<AuthMethod[]> {
+    let response: AuthMethodsReturn
+
+    if ('jwt' in proof) {
+      response = await this.guard.authMethods({}, { Authorization: `BEARER ${proof.jwt}` })
+    } else {
+      const signedProof = await signOwnershipProof(proof)
+
+      response = await this.guard.authMethods({
+        proof: {
+          wallet: signedProof.walletAddress,
+          timestamp: signedProof.timestamp.getTime(),
+          signer: signedProof.signerAddress,
+          signature: signedProof.signature
+        }
+      })
+    }
+
+    return response.methods.map(parseAuthMethod)
+  }
+
+  async setPin(pin: string | undefined, proof: AuthUpdateProof): Promise<void> {
+    const signedProof = await signAuthUpdateProof(proof)
+
+    if (pin === undefined) {
+      await this.guard.resetPIN(
+        { timestamp: signedProof.timestamp.getTime(), signature: signedProof.signature },
+        { Authorization: `BEARER ${proof.jwt}` }
+      )
+    } else {
+      await this.guard.setPIN(
+        { pin, timestamp: signedProof.timestamp.getTime(), signature: signedProof.signature },
+        { Authorization: `BEARER ${proof.jwt}` }
+      )
+    }
+  }
+
+  resetPin(proof: AuthUpdateProof): Promise<void> {
+    return this.setPin(undefined, proof)
+  }
+
+  async createTotp(proof: AuthUpdateProof): Promise<URL> {
+    const signedProof = await signAuthUpdateProof(proof)
+
+    const { uri } = await this.guard.createTOTP(
+      { timestamp: signedProof.timestamp.getTime(), signature: signedProof.signature },
+      { Authorization: `BEARER ${proof.jwt}` }
+    )
+
+    return new URL(uri)
+  }
+
+  async commitTotp(token: string, jwt: string): Promise<RecoveryCode[]> {
+    const { codes } = await this.guard.commitTOTP({ token }, { Authorization: `BEARER ${jwt}` })
+    return codes
+  }
+
+  async resetTotp(proof: AuthUpdateProof): Promise<void> {
+    const signedProof = await signAuthUpdateProof(proof)
+
+    await this.guard.resetTOTP(
+      { timestamp: signedProof.timestamp.getTime(), signature: signedProof.signature },
+      { Authorization: `BEARER ${proof.jwt}` }
+    )
+  }
+
+  async reset2fa(recoveryCode: string, proof: OwnershipProof): Promise<void> {
+    if ('jwt' in proof) {
+      await this.guard.reset2FA({ code: recoveryCode }, { Authorization: `BEARER ${proof.jwt}` })
+    } else {
+      const signedProof = await signOwnershipProof(proof)
+
+      await this.guard.reset2FA({
+        code: recoveryCode,
+        proof: {
+          wallet: signedProof.walletAddress,
+          timestamp: signedProof.timestamp.getTime(),
+          signer: signedProof.signerAddress,
+          signature: signedProof.signature
+        }
+      })
+    }
+  }
+
+  async getRecoveryCodes(proof: AuthUpdateProof): Promise<RecoveryCode[]> {
+    const signedProof = await signAuthUpdateProof(proof)
+
+    const { codes } = await this.guard.recoveryCodes(
+      { timestamp: signedProof.timestamp.getTime(), signature: signedProof.signature },
+      { Authorization: `BEARER ${proof.jwt}` }
+    )
+
+    return codes
+  }
+
+  async resetRecoveryCodes(proof: AuthUpdateProof): Promise<RecoveryCode[]> {
+    const signedProof = await signAuthUpdateProof(proof)
+
+    const { codes } = await this.guard.resetRecoveryCodes(
+      { timestamp: signedProof.timestamp.getTime(), signature: signedProof.signature },
+      { Authorization: `BEARER ${proof.jwt}` }
+    )
+
+    return codes
+  }
+
   private packMsgAndSig(address: string, msg: BytesLike, sig: BytesLike, chainId: ethers.BigNumberish): string {
     return ethers.utils.defaultAbiCoder.encode(['address', 'uint256', 'bytes', 'bytes'], [address, chainId, msg, sig])
   }
@@ -89,7 +197,7 @@ export class GuardSigner implements signers.SapientSigner {
     id: string,
     message: BytesLike,
     _: Status,
-    metadata: commons.WalletSignRequestMetadata
+    metadata: commons.WalletSignRequestMetadata & { guardTotpCode?: string }
   ): Promise<void> {
     // Building auxData, notice: this uses the old v1 format
     // TODO: We should update the guard API so we can pass the metadata directly
@@ -111,7 +219,13 @@ export class GuardSigner implements signers.SapientSigner {
           msg: ethers.utils.hexlify(message),
           auxData: this.packMsgAndSig(metadata.address, metadata.digest, encoded, metadata.chainId),
           chainId: ethers.BigNumber.from(metadata.chainId).toNumber() // TODO: This should be a string (in the API)
-        }
+        },
+        token: metadata.guardTotpCode
+          ? {
+              id: AuthMethod.TOTP,
+              token: metadata.guardTotpCode
+            }
+          : undefined
       })
 
       if (ethers.utils.arrayify(result.sig).length !== 0) {
@@ -129,4 +243,134 @@ export class GuardSigner implements signers.SapientSigner {
   suffix(): BytesLike {
     return this.appendSuffix ? [3] : []
   }
+}
+
+export type RecoveryCode = GuardRecoveryCode
+
+export enum AuthMethod {
+  PIN = 'PIN',
+  TOTP = 'TOTP'
+}
+
+function parseAuthMethod(method: string): AuthMethod {
+  switch (method) {
+    case AuthMethod.PIN:
+    case AuthMethod.TOTP:
+      return method
+    default:
+      throw new Error(`unknown auth method '${method}'`)
+  }
+}
+
+export type OwnershipProof =
+  | { jwt: string }
+  | {
+      walletAddress: string
+      timestamp: Date
+      signerAddress: string
+      signature: string
+    }
+  | {
+      walletAddress: string
+      signer: ethers.Signer | signers.SapientSigner
+    }
+
+function isSignedOwnershipProof(
+  proof: OwnershipProof
+): proof is { walletAddress: string; timestamp: Date; signerAddress: string; signature: string } {
+  return 'signerAddress' in proof && typeof proof.signerAddress === 'string'
+}
+
+async function signOwnershipProof(
+  proof: Exclude<OwnershipProof, { jwt: string }>
+): Promise<{ walletAddress: string; timestamp: Date; signerAddress: string; signature: string }> {
+  if (isSignedOwnershipProof(proof)) {
+    return proof
+  } else {
+    const signer = signers.isSapientSigner(proof.signer) ? proof.signer : new signers.SignerWrapper(proof.signer)
+    const signerAddress = await signer.getAddress()
+    const timestamp = new Date()
+    const typedData = getOwnershipProofTypedData(proof.walletAddress, timestamp)
+    const digest = encodeTypedDataDigest(typedData)
+    const randomId = ethers.utils.hexlify(ethers.utils.randomBytes(32))
+
+    return new Promise((resolve, reject) =>
+      signer.requestSignature(
+        randomId,
+        digest,
+        {},
+        {
+          onSignature(signature) {
+            resolve({
+              walletAddress: proof.walletAddress,
+              timestamp,
+              signerAddress,
+              signature: ethers.utils.hexlify(signature)
+            })
+          },
+          onRejection: reject,
+          onStatus(_situation) {}
+        }
+      )
+    )
+  }
+}
+
+export type AuthUpdateProof = { jwt: string } & ({ timestamp: Date; signature: string } | { wallet: Account })
+
+async function signAuthUpdateProof(proof: AuthUpdateProof): Promise<{ jwt: string; timestamp: Date; signature: string }> {
+  if ('wallet' in proof) {
+    const timestamp = new Date()
+    const typedData = getAuthUpdateProofTypedData(timestamp)
+
+    const signature = await proof.wallet.signTypedData(
+      typedData.domain,
+      typedData.types,
+      typedData.message,
+      typedData.domain.chainId ?? 1,
+      'eip6492'
+    )
+
+    return { jwt: proof.jwt, timestamp, signature }
+  } else {
+    return proof
+  }
+}
+
+export function getOwnershipProofTypedData(wallet: string, timestamp: Date): TypedData {
+  return {
+    domain,
+    types: {
+      AuthMethods: [
+        { name: 'wallet', type: 'address' },
+        { name: 'timestamp', type: 'string' }
+      ]
+    },
+    message: {
+      wallet: ethers.utils.getAddress(wallet),
+      timestamp: toUTCString(timestamp)
+    }
+  }
+}
+
+export function getAuthUpdateProofTypedData(timestamp: Date): TypedData {
+  return {
+    domain,
+    types: {
+      AuthUpdate: [{ name: 'timestamp', type: 'string' }]
+    },
+    message: {
+      timestamp: toUTCString(timestamp)
+    }
+  }
+}
+
+const domain: TypedDataDomain = {
+  name: 'Sequence Guard',
+  version: '1',
+  chainId: 1
+}
+
+function toUTCString(date: Date): string {
+  return date.toUTCString().replace('GMT', 'UTC')
 }
