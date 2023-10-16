8814e32d75682e737b07e2272b21be8777c4ecc6
Agusx1211 <agusgit@pm.me>
2023-10-16 16:28:32 +0000

    Add defaults and auth config

---
+++ packages/waas/src/defaults.ts
@@ -0,0 +1,11 @@
+
+export const DEFAULTS = {
+  guard: 'https://guard2.sequence.app',
+  auth: {
+    rpcServer: 'https://d33b7so3u0i8ki.cloudfront.net',
+    kmsRegion: 'us-east-2',
+    idpRegion: 'us-east-2',
+    keyId: '',
+    identityPoolId: '',
+  }
+}
