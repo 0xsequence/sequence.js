fd795d0b3d07f6feea6d6018995bd8e61204158a
Corban Riley <corbanbrook@gmail.com>
2024-02-23 11:59:50 -0500

    Fixing Wallet contexts

--- packages/tests/src/context/index.ts
+++ packages/tests/src/context/index.ts
@@ -2,8 +2,9 @@ import { ethers } from 'ethers'
 
 import { deployV1Context } from './v1'
 import { deployV2Context } from './v2'
+import { VersionedContext } from '@0xsequence/core/src/commons/context'
 
-export async function deploySequenceContexts(signer: ethers.Signer) {
+export async function deploySequenceContexts(signer: ethers.Signer): Promise<VersionedContext> {
   const v1 = await deployV1Context(signer)
   const v2 = await deployV2Context(signer)
   return { 1: v1, 2: v2 }
