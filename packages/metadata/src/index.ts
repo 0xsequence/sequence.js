8dacd4a1d3c919d42d51df392148b68f9ebb1d5e
Peter Kieltyka <peter.kieltyka@gmail.com>
2024-03-13 15:47:06 -0400

    metadata: SequenceCollections client

--- packages/metadata/src/index.ts
+++ packages/metadata/src/index.ts
@@ -1,6 +1,9 @@
 export * from './metadata.gen'
 
-import { Metadata as MetadataRpc } from './metadata.gen'
+import {
+  Metadata as MetadataRpc,
+  Collections as CollectionsRpc,
+} from './metadata.gen'
 
 const fetch = typeof global === 'object' ? global.fetch : window.fetch
 
@@ -16,7 +19,7 @@ export class SequenceMetadata extends MetadataRpc {
 
   _fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
     // automatically include jwt and access key auth header to requests
-    // if its been set on the api client
+    // if its been set on the client
     const headers: { [key: string]: any } = {}
 
     const jwtAuth = this.jwtAuth
@@ -35,4 +38,34 @@ export class SequenceMetadata extends MetadataRpc {
 
     return fetch(input, init)
   }
+}
+
+export class SequenceCollections extends CollectionsRpc {
+  constructor(
+    hostname: string = 'https://metadata.sequence.app',
+    public jwtAuth?: string
+  ) {
+    super(hostname.endsWith('/') ? hostname.slice(0, -1) : hostname, fetch)
+    this.fetch = this._fetch
+  }
+
+  _fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
+    // automatically include jwt auth header to requests
+    // if its been set on the client
+    const headers: { [key: string]: any } = {}
+
+    const jwtAuth = this.jwtAuth
+
+    if (jwtAuth && jwtAuth.length > 0) {
+      headers['Authorization'] = `BEARER ${jwtAuth}`
+    }
+
+    // before the request is made
+    init!.headers = { ...init!.headers, ...headers }
+
+    return fetch(input, init)
+  }
+
+  // TODO: add uploadAsset() method similar to,
+  // https://github.com/0xsequence/go-sequence/blob/master/metadata/collections.go#L52
 }
