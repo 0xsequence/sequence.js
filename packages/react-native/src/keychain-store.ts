ff0699ef61ce1be4eced41e1c2a9ae154cdb4f54
Taylan Pince <taylanpince@gmail.com>
2024-04-28 16:35:28 +0200

    Ignore react-native for linting

--- packages/react-native/src/keychain-store.ts
+++ packages/react-native/src/keychain-store.ts
@@ -1,16 +1,14 @@
 import { SecureStoreBackend } from '@0xsequence/waas'
 
-import * as Keychain from 'react-native-keychain'
+import { getGenericPassword, setGenericPassword, resetGenericPassword } from 'react-native-keychain'
 
 export class KeychainSecureStoreBackend implements SecureStoreBackend {
     constructor() {
-        if (typeof Keychain !== 'object') {
-            throw new Error('Keychain is not available')
-        }
+        // no-op
     }
 
     async get(dbName: string, dbStoreName: string, key: string): Promise<string | null> {
-        const credentials = await Keychain.getGenericPassword({ service: dbStoreName })
+        const credentials = await getGenericPassword({ service: dbStoreName })
         if (credentials) {
             return credentials.password
         } else {
@@ -19,11 +17,11 @@ export class KeychainSecureStoreBackend implements SecureStoreBackend {
     }
 
     async set(dbName: string, dbStoreName: string, key: string, value: string): Promise<boolean> {
-        await Keychain.setGenericPassword(key, value, { service: dbStoreName })
+        await setGenericPassword(key, value, { service: dbStoreName })
         return true
     }
 
     async delete(dbName: string, dbStoreName: string, key: string): Promise<boolean> {
-        return Keychain.resetGenericPassword({ service: dbStoreName })
+        return resetGenericPassword({ service: dbStoreName })
     }
 }
