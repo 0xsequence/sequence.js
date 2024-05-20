31000da7aa2b728273456fb4cd35d088d87fd367
José Nieto <jose@xiam.io>
2024-05-20 13:55:06 -0600

    add SequenceIndexerOpts

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -4,13 +4,40 @@ import { Indexer as IndexerRpc } from './indexer.gen'
 
 const fetch = typeof global === 'object' ? global.fetch : window.fetch
 
+export interface SequenceIndexerOpts {
+  JWTAuth?: string
+  ProjectAccessKey?: string
+}
+
 export class SequenceIndexer extends IndexerRpc {
+  public jwtAuth?: string
+  public projectAccessKey?: string
+
   constructor(
     hostname: string,
-    public projectAccessKey?: string,
-    public jwtAuth?: string
+    opts: SequenceIndexerOpts
+  );
+  constructor(
+    hostname: string,
+    projectAccessKey?: string,
+    jwtAuth?: string
+  );
+  constructor(
+    hostname: string,
+    ...args: any[]
   ) {
     super(hostname.endsWith('/') ? hostname.slice(0, -1) : hostname, fetch)
+
+    if (args.length === 1 && typeof args[0] === 'object') {
+      const opts = args[0] as SequenceIndexerOpts
+      this.projectAccessKey = opts.ProjectAccessKey
+      this.jwtAuth = opts.JWTAuth
+    } else {
+      const [projectAccessKey, jwtAuth] = args
+      this.projectAccessKey = projectAccessKey || undefined
+      this.jwtAuth = jwtAuth || undefined
+    }
+
     this.fetch = this._fetch
   }
 
@@ -18,6 +45,7 @@ export class SequenceIndexer extends IndexerRpc {
     // automatically include jwt and access key auth header to requests
     // if its been set on the api client
     const headers: { [key: string]: any } = {}
+
 
     const jwtAuth = this.jwtAuth
     const projectAccessKey = this.projectAccessKey
