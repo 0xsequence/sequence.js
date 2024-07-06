c78bbcc8309edc3fc08ca8cf59cf710a33628d19
piotr <piotr@layerzerolabs.org>
2024-07-06 23:27:17 +0200

    feat: multicall3 support

--- packages/multicall/src/constants.ts
+++ packages/multicall/src/constants.ts
@@ -1,5 +1,7 @@
 export enum JsonRpcMethod {
   ethCall = 'eth_call',
   ethGetBalance = 'eth_getBalance',
-  ethGetCode = 'eth_getCode'
+  ethGetCode = 'eth_getCode', // not used in multicall3
+  ethGetChainId = 'eth_chainId',
+  ethBlockNumber = 'eth_blockNumber',
 }
