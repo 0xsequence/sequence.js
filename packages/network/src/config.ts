12d9f1e70287c794a37d8fa48a295b0bf56e4d1f
Taylan Pince <taylanpince@gmail.com>
2024-11-06 16:00:57 +0100

    network: Add The Root Network Porcini + Mainnet (#604)

--- packages/network/src/config.ts
+++ packages/network/src/config.ts
@@ -130,5 +130,7 @@ export const allNetworks = validateAndSortNetworks([
   createNetworkConfig(ChainId.TOY_TESTNET),
   createNetworkConfig(ChainId.IMMUTABLE_ZKEVM),
   createNetworkConfig(ChainId.IMMUTABLE_ZKEVM_TESTNET),
+  createNetworkConfig(ChainId.ROOT_NETWORK),
+  createNetworkConfig(ChainId.ROOT_NETWORK_PORCINI),
   ...hardhatNetworks
 ])
