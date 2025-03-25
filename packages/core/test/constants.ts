5091dac9e95d56efb8cfe463f814fb51be824b8f
Michael Standen <mstan@horizon.io>
2025-03-26 09:53:20 +1300

    Update deployed contract addresses

--- packages/core/test/constants.ts
+++ packages/core/test/constants.ts
@@ -4,9 +4,9 @@ import { Abi, Address } from 'ox'
 const envFile = process.env.CI ? '.env.test' : '.env.test.local'
 dotenvConfig({ path: envFile })
 
-export const ERC20_IMPLICIT_MINT_CONTRACT: Address.Address = '0x7aC9f16587B279998f1522f8F737a28aCb4C9f06'
-export const MOCK_IMPLICIT_CONTRACT: Address.Address = '0x9D5E1139e02eB025470308EeF65D2d15bC1f5d9F'
-export const MOCK_IMPLICIT_INVALID_CONTRACT: Address.Address = '0x977F7fbCAc606C3B33357ebDff0205B8C4055a3B'
+export const ERC20_IMPLICIT_MINT_CONTRACT: Address.Address = '0x96ea29a63fdCf897eDc059a7f7A7bf04Bf574eF3'
+export const MOCK_IMPLICIT_CONTRACT: Address.Address = '0x95b45E3131e836Ed3773C169c2A7E0C52478F1C6'
+export const MOCK_IMPLICIT_INVALID_CONTRACT: Address.Address = '0x4cFD26fBADCeef5dA7e1D1BF4894a36FdaDfA3d6'
 
 export const ERC20_MINT_ONCE = Abi.from(['function mintOnce(address to, uint256 amount)'])[0]
 
