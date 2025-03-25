e635544226ef0a22293b70c072197b1ad5bd7522
Michael Standen <mstan@horizon.io>
2025-03-26 08:19:23 +1300

    Update default addresses

--- packages/primitives/src/context.ts
+++ packages/primitives/src/context.ts
@@ -4,11 +4,13 @@ import { DEFAULT_CREATION_CODE } from './constants'
 export type Context = {
   factory: Address.Address
   stage1: Address.Address
+  // stage2: Address.Address
   creationCode: Hex.Hex
 }
 
 export const Dev1: Context = {
   factory: '0xBd0F8abD58B4449B39C57Ac9D5C67433239aC447',
-  stage1: '0x7CC3CD2a7c2611e8bE6BAFfe17d0fA9B15a8858d',
+  stage1: '0x76E132217927D8B0d1374328adC48Bd2b7EEC8d7',
+  // stage2: '0xb1ED343aAaC68F800B66b66c63F87211BEF8Bb97',
   creationCode: DEFAULT_CREATION_CODE,
 }
