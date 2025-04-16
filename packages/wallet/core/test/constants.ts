b8bf4f1b7a8d49ebe3d6bfb8a16bb66e21d286e7
Michael Standen <mstan@horizon.io>
2025-04-16 12:31:32 +1200

    Simplify session tests

--- packages/wallet/core/test/constants.ts
+++ packages/wallet/core/test/constants.ts
@@ -4,11 +4,8 @@ import { Abi, Address } from 'ox'
 const envFile = process.env.CI ? '.env.test' : '.env.test.local'
 dotenvConfig({ path: envFile })
 
-export const MOCK_IMPLICIT_CONTRACT: Address.Address = '0x041E0CDC028050519C8e6485B2d9840caf63773F'
-export const MOCK_IMPLICIT_INVALID_CONTRACT: Address.Address = '0x99aA13abCDB1759Eb8653fB12090BA95bd793083'
-export const ERC20_IMPLICIT_MINT_CONTRACT: Address.Address = '0xe19D4dBC90e371c4adC42f07344C2C9a50838d84'
-
-export const ERC20_MINT_ONCE = Abi.from(['function mintOnce(address to, uint256 amount)'])[0]
+export const EMITTER_ADDRESS = '0xb9239d78e60F3491b36538C6e51916c7B552cCBb'
+export const EMITTER_ABI = Abi.from(['function explicitEmit()', 'function implicitEmit()'])
 
 // Environment variables
 export const { RPC_URL, PRIVATE_KEY } = process.env
--- packages/wallet/wdk/test/constants.ts
+++ packages/wallet/wdk/test/constants.ts
@@ -4,11 +4,8 @@ import { Abi } from 'ox'
 const envFile = process.env.CI ? '.env.test' : '.env.test.local'
 dotenvConfig({ path: envFile })
 
-export const ERC20_IMPLICIT_MINT_CONTRACT = '0x96ea29a63fdCf897eDc059a7f7A7bf04Bf574eF3'
-export const MOCK_IMPLICIT_CONTRACT = '0x95b45E3131e836Ed3773C169c2A7E0C52478F1C6'
-export const MOCK_IMPLICIT_INVALID_CONTRACT = '0x4cFD26fBADCeef5dA7e1D1BF4894a36FdaDfA3d6'
-
-export const ERC20_MINT_ONCE = Abi.from(['function mintOnce(address to, uint256 amount)'])[0]
+export const EMITTER_ADDRESS = '0xb9239d78e60F3491b36538C6e51916c7B552cCBb'
+export const EMITTER_ABI = Abi.from(['function explicitEmit()', 'function implicitEmit()'])
 
 // Environment variables
 export const { RPC_URL, PRIVATE_KEY } = process.env
