d0b856e68b926cf546bde3722c19368c4a78b579
Shun Kakinoki <shunkakinoki@gmail.com>
2025-05-02 23:49:21 +0900

    Update default stage addresses in HomeIndexRoute, AnyPay tests, and constants for consistency across the application. Ensure alignment with recent changes in address verification and intent configuration.

--- extras/demo-anypay/src/routes/home/home-index-route.tsx
+++ extras/demo-anypay/src/routes/home/home-index-route.tsx
@@ -189,8 +189,8 @@ export const HomeIndexRoute = () => {
     try {
       const context: ContextLike.Context = {
         factory: '0xBd0F8abD58B4449B39C57Ac9D5C67433239aC447' as `0x${string}`,
-        stage1: '0x2440595Ead70Ba5874572153910362DcA2dde417' as `0x${string}`,
-        stage2: '0xa3F27508a1Dac8A11C0791f7EBEA5fc95dC1e131' as `0x${string}`,
+        stage1: '0x53bA242E7C2501839DF2972c75075dc693176Cd0' as `0x${string}`,
+        stage2: '0xa29874c88b8Fd557e42219B04b0CeC693e1712f5' as `0x${string}`,
         creationCode:
           '0x603e600e3d39601e805130553df33d3d34601c57363d3d373d363d30545af43d82803e903d91601c57fd5bf3' as `0x${string}`,
       }
