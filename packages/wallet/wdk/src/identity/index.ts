ac8d4f66805896ac5a6d2ff74b19cbc7058e38e5
Patryk Kalinowski <patryk@kalinowski.dev>
2025-05-15 19:20:24 +0200

    wdk/identity: use the new identity-instrument package

--- packages/wallet/wdk/src/identity/index.ts
+++
@@ -1,4 +0,0 @@
-export { IdentityInstrument, IdentityType, KeyType } from './nitro/index.js'
-export type { CommitChallengeParams, CompleteChallengeParams, Challenge } from './challenge.js'
-export { IdTokenChallenge, AuthCodeChallenge, AuthCodePkceChallenge, OtpChallenge } from './challenge.js'
-export { IdentitySigner } from './signer.js'
