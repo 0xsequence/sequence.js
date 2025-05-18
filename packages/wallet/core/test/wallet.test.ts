9eefe9a61a6ca25252aa9615b643da1435a69477
Michael Standen <mstan@horizon.io>
2025-05-19 11:52:01 +1200

    Add 6492 validation

--- packages/wallet/core/test/wallet.test.ts
+++ packages/wallet/core/test/wallet.test.ts
@@ -1,8 +1,7 @@
-import { AbiFunction, Address, Bytes, Hash, Hex, Provider, RpcTransport, Secp256k1 } from 'ox'
+import { Address, Hash, Hex, Provider, RpcTransport, Secp256k1 } from 'ox'
 import { describe, expect, it } from 'vitest'
 
-import { IS_VALID_SIGNATURE } from '../../primitives/src/constants.js'
-import { Config, Payload } from '../../primitives/src/index.js'
+import { Config, Erc6492, Payload } from '../../primitives/src/index.js'
 import { Envelope, State, Wallet } from '../src/index.js'
 import { LOCAL_RPC_URL } from './constants.js'
 
@@ -15,10 +14,12 @@ describe('Wallet', async () => {
     return { address, privateKey }
   }
 
-  describe('deployed', async () => {
-    const getDeployedWallet = async (config: Config.Config, provider: Provider.Provider) => {
+  const types = ['deployed', 'not-deployed']
+
+  for (const type of types) {
+    const getWallet = async (config: Config.Config, provider: Provider.Provider, deployed: boolean) => {
       const wallet = await Wallet.fromConfiguration(config, { stateProvider })
-      if (!(await wallet.isDeployed(provider))) {
+      if (deployed && !(await wallet.isDeployed(provider))) {
         // Deploy it
         const deployTransaction = await wallet.buildDeployTransaction()
         const deployResult = await provider.request({
@@ -31,63 +32,54 @@ describe('Wallet', async () => {
         })
       }
       const isDeployed = await wallet.isDeployed(provider)
-      expect(isDeployed).toBe(true)
+      expect(isDeployed).toBe(deployed)
       return wallet
     }
 
-    it('should sign a message', async () => {
-      const provider = Provider.from(RpcTransport.fromHttp(LOCAL_RPC_URL))
-      const chainId = BigInt(await provider.request({ method: 'eth_chainId' }))
+    describe(type, async () => {
+      it('should sign a message', async () => {
+        const provider = Provider.from(RpcTransport.fromHttp(LOCAL_RPC_URL))
+        const chainId = BigInt(await provider.request({ method: 'eth_chainId' }))
 
-      const signer = createRandomSigner()
-      const wallet = await getDeployedWallet(
-        {
-          threshold: 1n,
-          checkpoint: 0n,
-          topology: { type: 'signer', address: signer.address, weight: 1n },
-        },
-        provider,
-      )
-
-      const message = Hex.fromString('Hello, world!')
-      const messageHash = Hash.keccak256(message)
-      console.log('messageHash', messageHash)
-
-      const envelope = await wallet.prepareMessageSignature(message, chainId)
-      const payloadHash = Payload.hash(wallet.address, chainId, envelope.payload)
-      console.log('payloadHash', Bytes.toHex(payloadHash))
-
-      // Sign it
-      const signerSignature = Secp256k1.sign({
-        payload: payloadHash,
-        privateKey: signer.privateKey,
-      })
-      const signedEnvelope = Envelope.toSigned(envelope, [
-        {
-          address: signer.address,
-          signature: {
-            type: 'hash',
-            ...signerSignature,
-          },
-        },
-      ])
-
-      // Encode it
-      const signature = await wallet.buildMessageSignature(signedEnvelope, provider)
-
-      // Validate it using IERC1271
-      const requestData = AbiFunction.encodeData(IS_VALID_SIGNATURE, [messageHash, Bytes.toHex(signature)])
-      const result = await provider.request({
-        method: 'eth_call',
-        params: [
+        const signer = createRandomSigner()
+        const wallet = await getWallet(
           {
-            to: wallet.address,
-            data: requestData,
+            threshold: 1n,
+            checkpoint: 0n,
+            topology: { type: 'signer', address: signer.address, weight: 1n },
           },
-        ],
+          provider,
+          type === 'deployed',
+        )
+
+        const message = Hex.fromString('Hello, world!')
+        const messageHash = Hash.keccak256(message)
+
+        const envelope = await wallet.prepareMessageSignature(message, chainId)
+        const payloadHash = Payload.hash(wallet.address, chainId, envelope.payload)
+
+        // Sign it
+        const signerSignature = Secp256k1.sign({
+          payload: payloadHash,
+          privateKey: signer.privateKey,
+        })
+        const signedEnvelope = Envelope.toSigned(envelope, [
+          {
+            address: signer.address,
+            signature: {
+              type: 'hash',
+              ...signerSignature,
+            },
+          },
+        ])
+
+        // Encode it
+        const signature = await wallet.buildMessageSignature(signedEnvelope, provider)
+
+        // Validate off chain with ERC-6492
+        const isValid = await Erc6492.isValid(wallet.address, messageHash, signature, provider)
+        expect(isValid).toBe(true)
       })
-      const decodedResult = AbiFunction.decodeResult(IS_VALID_SIGNATURE, result)
-      expect(decodedResult).toBe(AbiFunction.getSelector(IS_VALID_SIGNATURE))
     })
-  })
+  }
 })
