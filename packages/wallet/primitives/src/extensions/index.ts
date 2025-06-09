3b9ce55e991ba61d42b0d3bc13285abbf4b0ce5a
Michael Standen <mstan@horizon.io>
2025-06-09 15:50:34 +1200

    Reduce deadline to uint64

--- packages/wallet/primitives/src/extensions/index.ts
+++ packages/wallet/primitives/src/extensions/index.ts
@@ -9,13 +9,13 @@ export type Extensions = {
 export const Dev1: Extensions = {
   passkeys: '0x8f26281dB84C18aAeEa8a53F94c835393229d296',
   recovery: '0xd98da48C4FF9c19742eA5856A277424557C863a6',
-  sessions: '0x44F27c3D1D6F3000Ea7F4c3ba3d378aE69c9566A',
+  sessions: '0xe5AB0D993c473bb75594248CCd13f9E073a23E9c',
 }
 
 export const Dev2: Extensions = {
   passkeys: '0x4491845806B757D67BE05BbD877Cab101B9bee5C',
   recovery: '0xdED857b9b5142832634129aFfc1D67cD106b927c',
-  sessions: '0x44F27c3D1D6F3000Ea7F4c3ba3d378aE69c9566A',
+  sessions: '0xe5AB0D993c473bb75594248CCd13f9E073a23E9c',
 }
 
 export * as Passkeys from './passkeys.js'
