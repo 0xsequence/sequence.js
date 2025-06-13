9b97753c51cac3decbcf7f2a2f0c2e227791e8e0
Corban Riley <corbanbrook@gmail.com>
2025-06-13 13:45:13 -0400

    Adding opStatus: Relayer.OperationStatus to TransactionRelayed

--- packages/wallet/wdk/src/sequence/transactions.ts
+++ packages/wallet/wdk/src/sequence/transactions.ts
@@ -274,10 +274,13 @@ export class Transactions {
       tx.relayerOption.quote,
     )
 
+    const opStatus = await relayer.status(opHash, tx.envelope.chainId)
+
     await this.shared.databases.transactions.set({
       ...tx,
       status: 'relayed',
       opHash,
+      opStatus,
     } as TransactionRelayed)
 
     await this.shared.modules.signatures.complete(signature.id)
--- packages/wallet/wdk/src/sequence/types/transaction-request.ts
+++ packages/wallet/wdk/src/sequence/types/transaction-request.ts
@@ -43,6 +43,7 @@ export type TransactionFormed = TransactionBase & {
 export type TransactionRelayed = TransactionBase & {
   status: 'relayed'
   opHash: string
+  opStatus: Relayer.OperationStatus
 }
 
 export type Transaction = TransactionRequested | TransactionDefined | TransactionFormed | TransactionRelayed
