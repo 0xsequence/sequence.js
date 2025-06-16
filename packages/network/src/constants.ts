61c285aefed77ce35b840789b0eee6b87b6574b4
andres-horizon <162160045+andres-horizon@users.noreply.github.com>
2025-06-16 18:06:07 +0000

    [AUTOMATED] Update constants.ts

--- packages/network/src/constants.ts
+++ packages/network/src/constants.ts
@@ -141,7 +141,11 @@ export enum ChainId {
   SOMNIA_TESTNET = 50312,
 
   // INCENTIV TESTNET
-  INCENTIV_TESTNET = 11690
+  INCENTIV_TESTNET = 11690,
+
+  //SEI
+  SEI = 1329,
+  SEI_TESTNET = 1328
 }
 
 export const networks: Record<ChainId, NetworkMetadata> = {
@@ -1131,6 +1135,42 @@ export const networks: Record<ChainId, NetworkMetadata> = {
     nativeToken: {
       symbol: 'CENT',
       name: 'CENT',
+      decimals: 18
+    }
+  },
+
+  [ChainId.SEI]: {
+    chainId: ChainId.SEI,
+    type: NetworkType.MAINNET,
+    name: 'sei',
+    title: 'Sei',
+    logoURI: `https://assets.sequence.info/images/networks/medium/${ChainId.SEI}.webp`,
+    testnet: false,
+    blockExplorer: {
+      name: 'SEI Explorer',
+      rootUrl: 'https://seitrace.com/?chain=pacific-1'
+    },
+    nativeToken: {
+      symbol: 'SEI',
+      name: 'SEI',
+      decimals: 18
+    }
+  },
+
+  [ChainId.SEI_TESTNET]: {
+    chainId: ChainId.SEI_TESTNET,
+    type: NetworkType.TESTNET,
+    name: 'sei-testnet',
+    title: 'Sei Testnet',
+    logoURI: `https://assets.sequence.info/images/networks/medium/${ChainId.SEI_TESTNET}.webp`,
+    testnet: true,
+    blockExplorer: {
+      name: 'Sei Testnet Explorer',
+      rootUrl: 'https://seitrace.com/?chain=atlantic-2'
+    },
+    nativeToken: {
+      symbol: 'SEI',
+      name: 'SEI',
       decimals: 18
     }
   }
