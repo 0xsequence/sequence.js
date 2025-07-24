4bb640335fb221607d117d6ca674a9766d166f75
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2025-07-24 14:26:33 +0300

    More exports

--- packages/wallet/dapp-client/src/index.ts
+++ packages/wallet/dapp-client/src/index.ts
@@ -7,6 +7,9 @@ export type {
   RequestActionType,
   SignatureResponse,
   ChainSessionManagerEvent,
+  SequenceSessionStorage,
+  RandomPrivateKeyFn,
+  Session,
 } from './types/index.js'
 export {
   FeeOptionError,
@@ -16,5 +19,12 @@ export {
   InitializationError,
   SigningError,
 } from './utils/errors.js'
-export { getExplorerUrl } from './utils/index.js'
+export { getExplorerUrl, jsonReplacers, jsonRevivers } from './utils/index.js'
+export type {
+  SequenceStorage,
+  ExplicitSessionData,
+  ImplicitSessionData,
+  SignatureRequestContext,
+  PendingRequestPayload,
+} from './utils/storage.js'
 export { WebStorage } from './utils/storage.js'
