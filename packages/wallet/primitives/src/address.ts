d3db14089327a8302169d4c53bce8d094cd9ec2c
William Hua <william@attente.ca>
2025-07-24 10:02:11 -0400

    Address.Checksummed

--- packages/wallet/primitives/src/address.ts
+++ packages/wallet/primitives/src/address.ts
@@ -1,6 +1,24 @@
-import { Bytes, Hash } from 'ox'
+import { Address, Bytes, Hash, PublicKey } from 'ox'
 import { Context } from './context.js'
 import { Config, hashConfiguration } from './config.js'
+
+export type Checksummed = Address.Address & { readonly _checksummed: unique symbol }
+
+export function checksum(address: string): Checksummed {
+  return Address.checksum(address) as Checksummed
+}
+
+export function isChecksummed(address: any): address is Checksummed {
+  return typeof address === 'string' && Address.validate(address) && address === Address.checksum(address)
+}
+
+export function isEqual(a: Checksummed, b: Checksummed): boolean {
+  return a === b
+}
+
+export function fromPublicKey(publicKey: PublicKey.PublicKey, options?: Address.fromPublicKey.Options): Checksummed {
+  return checksum(Address.fromPublicKey(publicKey, options))
+}
 
 export function from(configuration: Bytes.Bytes | Config, context: Omit<Context, 'stage2'>): Checksummed {
   const imageHash = configuration instanceof Uint8Array ? configuration : hashConfiguration(configuration)
