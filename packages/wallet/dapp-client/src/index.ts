7ee2a872b4f0305fc7422c6bedd62f2f47bb6292
tolgahan-arikan <tolgahan.arikan@gmail.com>
2025-07-24 14:27:43 +0300

    Update dapp-client exports (#822)
    * Update dapp-client exports
    
    * More exports

--- packages/wallet/dapp-client/src/index.ts
+++ packages/wallet/dapp-client/src/index.ts
@@ -1,1 +1,30 @@
-export * as DappClient from './DappClient.js'
+export { DappClient } from './DappClient.js'
+export type { DappClientEventListener, DappClientSignatureEventListener } from './DappClient.js'
+export type {
+  PreferredLoginMethod,
+  TransportMode,
+  Transaction,
+  RequestActionType,
+  SignatureResponse,
+  ChainSessionManagerEvent,
+  SequenceSessionStorage,
+  RandomPrivateKeyFn,
+  Session,
+} from './types/index.js'
+export {
+  FeeOptionError,
+  TransactionError,
+  AddExplicitSessionError,
+  ConnectionError,
+  InitializationError,
+  SigningError,
+} from './utils/errors.js'
+export { getExplorerUrl, jsonReplacers, jsonRevivers } from './utils/index.js'
+export type {
+  SequenceStorage,
+  ExplicitSessionData,
+  ImplicitSessionData,
+  SignatureRequestContext,
+  PendingRequestPayload,
+} from './utils/storage.js'
+export { WebStorage } from './utils/storage.js'
