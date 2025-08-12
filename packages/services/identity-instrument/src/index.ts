83c0fd3e8d1bf99f7d571244bf3333c6d4f0969c
Patryk Kalinowski <patryk@kalinowski.dev>
2025-08-12 14:31:17 +0200

    use identity instrument signer scoping

--- packages/services/identity-instrument/src/index.ts
+++ packages/services/identity-instrument/src/index.ts
@@ -15,14 +15,19 @@ export { KeyType, IdentityType, AuthMode }
 export * from './challenge.js'
 
 export class IdentityInstrument {
+  private scope?: string
   private rpc: IdentityInstrumentRpc
 
-  constructor(hostname: string, fetch = window.fetch) {
+  constructor(hostname: string, scope?: string, fetch = window.fetch) {
     this.rpc = new IdentityInstrumentRpc(hostname.endsWith('/') ? hostname.slice(0, -1) : hostname, fetch)
+    this.scope = scope
   }
 
   async commitVerifier(authKey: AuthKey, challenge: Challenge) {
-    const params = challenge.getCommitParams()
+    const params = {
+      ...challenge.getCommitParams(),
+      scope: this.scope,
+    }
     const signature = await authKey.sign(Bytes.fromString(canonicalize(params)))
     return this.rpc.commitVerifier({
       params,
@@ -38,6 +43,7 @@ export class IdentityInstrument {
     const params = {
       ...challenge.getCompleteParams(),
       signerType: KeyType.Ethereum_Secp256k1,
+      scope: this.scope,
     }
     const signature = await authKey.sign(Bytes.fromString(canonicalize(params)))
     return this.rpc.completeAuth({
@@ -52,6 +58,7 @@ export class IdentityInstrument {
 
   async sign(authKey: AuthKey, digest: Bytes.Bytes) {
     const params = {
+      scope: this.scope,
       signer: {
         address: authKey.signer,
         keyType: KeyType.Ethereum_Secp256k1,
