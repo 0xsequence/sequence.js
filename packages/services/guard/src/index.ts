ac72a35f37508375f87e4919cf1725c4e47dbbc0
Patryk Kalinowski <patryk@kalinowski.dev>
2025-09-03 16:46:30 +0200

    guard: return a specific error when auth required

--- packages/services/guard/src/index.ts
+++ packages/services/guard/src/index.ts
@@ -1,5 +1,6 @@
 export * from './types.js'
 export { PayloadType, SignatureType, type Signature } from './client/guard.gen.js'
 
+export * as Client from './client/guard.gen.js'
 export * as Sequence from './sequence.js'
 export * as Local from './local.js'
