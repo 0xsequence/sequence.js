c8ef48ca0e1f1c3eab542797ce14b23b29b44ab2
VGabriel45 <vgabrielmarian21@gmail.com>
2025-09-08 12:56:34 +0300

    added waitForReceipt to AccountSigner.sendTransaction

--- packages/account/src/signer.ts
+++ packages/account/src/signer.ts
@@ -149,6 +149,7 @@ export class AccountSigner implements ethers.AbstractSigner<ethers.Provider> {
     txs: commons.transaction.Transactionish,
     options?: {
       simulateForFeeOptions?: boolean
+      waitForReceipt?: boolean
     }
   ): Promise<ethers.TransactionResponse> {
     const prepare = await this.account.prepareTransactions({
@@ -169,11 +170,10 @@ export class AccountSigner implements ethers.AbstractSigner<ethers.Provider> {
       prepare.feeQuote,
       undefined,
       undefined,
-      this.options?.nonceSpace !== undefined
-        ? {
-            nonceSpace: this.options.nonceSpace
-          }
-        : undefined
+      {
+        nonceSpace: this.options?.nonceSpace,
+        waitForReceipt: options?.waitForReceipt
+      }
     ) as Promise<ethers.TransactionResponse> // Will always have a transaction response
   }
 
