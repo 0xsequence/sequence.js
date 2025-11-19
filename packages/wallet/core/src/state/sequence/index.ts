5db652082e59d5e36cf99fde5472a110a04621c9
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2025-11-19 15:35:45 +0300

    Update keymachine url in Provider constructor

--- packages/wallet/core/src/state/sequence/index.ts
+++ packages/wallet/core/src/state/sequence/index.ts
@@ -14,7 +14,7 @@ import { Sessions, SignatureType } from './sessions.gen.js'
 export class Provider implements ProviderInterface {
   private readonly service: Sessions
 
-  constructor(host = 'https://v3-keymachine.sequence-dev.app') {
+  constructor(host = 'https://keymachine.sequence.app') {
     this.service = new Sessions(host, fetch)
   }
 
