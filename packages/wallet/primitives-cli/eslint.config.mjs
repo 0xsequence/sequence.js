fd943edbb8b59b706ebaaebcb0018f74f1e8d0d3
Agusx1211 <agusgit@pm.me>
2025-11-20 16:25:31 +0000

    Expose primitives CLI bin and use base lint config

--- packages/wallet/primitives-cli/eslint.config.mjs
+++ packages/wallet/primitives-cli/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config as baseConfig } from "@repo/eslint-config/base"
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default baseConfig
